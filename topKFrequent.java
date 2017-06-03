11558f3d4128f796b133090f5aa4f15cdd2e1536
jialuyyy <janeyuan1992@gmail.com>
2017-06-03 17:32:19 -0400

    Update topKFrequent.java

--- topKFrequent.java
+++ topKFrequent.java
@@ -65,3 +65,41 @@ public class Solution {
         return ret;
     }
 }
+
+//beats 74.96%
+//bucket sort method from the leetcode discussion
+//1.use a hashmap to maintain the number and its frequency.
+//2.create a list array and its index is corresponding to the frequency. put the keys into the corresponding list according to its frequency.
+//Time Complexity: O(n)
+public class Solution {
+    public List<Integer> topKFrequent(int[] nums, int k) {
+        List<Integer> ret = new ArrayList<Integer>();
+        if (nums == null || nums.length == 0) {
+            return ret;
+        }
+        
+        Map<Integer, Integer> hashMap = new HashMap<Integer, Integer>();
+        for (int num: nums) {
+            hashMap.put(num, hashMap.getOrDefault(num, 0) + 1);
+        }
+        
+        List[] buckets = new List[nums.length + 1];
+        
+        for (int key: hashMap.keySet()) {
+            int frequency = hashMap.get(key);
+            
+            if (buckets[frequency] == null) {
+                buckets[frequency] = new ArrayList<Integer>();
+            }
+            
+            buckets[frequency].add(key);
+        }
+        
+        for (int i = buckets.length - 1; i >= 0 && ret.size() < k; i--) {
+            if (buckets[i] != null)
+                ret.addAll(buckets[i]);
+        }
+        
+        return ret.subList(0, k);
+    }
+}
