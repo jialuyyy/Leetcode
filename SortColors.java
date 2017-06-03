ab584f640b02f38dee508c66b8c8d806c2a83665
jialuyyy <janeyuan1992@gmail.com>
2017-06-03 19:56:04 -0400

    Update SortColors.java

--- SortColors.java
+++ SortColors.java
@@ -5,6 +5,46 @@ Here, we will use the integers 0, 1, and 2 to represent the color red, white, an
 Note:
 You are not suppose to use the library's sort function for this problem.*/
 
+//premitive solution: count sorting
+//beats 5.83%
+//very straightforward, just count the numbers of every color and put them into the array in sequence.
+//Time Complexity: two times iteration O(2n)
+public class Solution {
+    public void sortColors(int[] nums) {
+        if (nums == null || nums.length < 2) {
+            return;
+        }
+        
+        int countRed = 0;
+        int countWhite = 0;
+        int countBlue = 0;
+        
+        for (int num: nums) {
+            if (num == 0) {
+                countRed++;
+            } else if (num == 1) {
+                countWhite++;
+            } else {
+                countBlue++;
+            }
+        }
+        
+        
+        for (int i = 0; i < countRed; i++) {
+            nums[i] = 0;
+        }
+        
+        for (int i = countRed; i < countRed + countWhite; i++) {
+            nums[i] = 1;
+        }
+        
+        for (int i = countRed + countWhite; i < nums.length; i++) {
+            nums[i] = 2;
+        }   
+    }
+}
+
+//optimized solution
 //beats: 61.41%
 //Time complexity: O(n)
 //use three pointers:
