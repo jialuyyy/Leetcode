56b12b05bba615653236b65f38696eaec15714b6
jialuyyy <janeyuan1992@gmail.com>
2017-06-03 19:09:18 -0400

    Update isSymmetric.java

--- isSymmetric.java
+++ isSymmetric.java
@@ -13,7 +13,7 @@ But the following [1,2,2,null,3,null,3] is not:
   2   2
    \   \
    3    3*/
-
+//DFS recursion
 //beats 24.85%
 //Time Complexity: O(n), as every node is visited once.
 //Space Complexity: O(n)
@@ -46,5 +46,43 @@ public class Solution {
         }
         
         return symmetric(left.left, right.right) && symmetric(left.right, right.left) && left.val == right.val;
+    }
+}
+
+
+//iterative solution(from discussion) breadth-first traversal
+//using queue
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.offer(root);
+        q.offer(root);
+        
+        while(!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            
+            q.offer(t1.left);
+            q.offer(t2.right);
+            q.offer(t1.right);
+            q.offer(t2.left);
+        }
+        
+        return true;
     }
 }
