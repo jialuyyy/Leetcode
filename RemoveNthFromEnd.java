4dbe7237228e0262c24c7620bc333ed512d4ddfa
jialuyyy <janeyuan1992@gmail.com>
2017-06-04 19:34:36 -0400

    Update RemoveNthFromEnd.java

--- RemoveNthFromEnd.java
+++ RemoveNthFromEnd.java
@@ -42,3 +42,36 @@ public class Solution {
         return dummy.next;
     }
 }
+
+//updates: cleaner and shorter one
+//beats: 30.55%
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        
+        ListNode slow = head;
+        ListNode fast = head;
+        while (n > 0 && fast != null) {
+            fast = fast.next;
+            n--;
+        }
+        
+        if (fast == null) {
+            return head.next;
+        }
+        while (fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+        
+        slow.next = slow.next.next;
+        return head;
+    }
+}
