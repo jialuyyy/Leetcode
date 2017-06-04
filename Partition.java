5c5291b2c26eef74eb3b297b1b1acac1ec0cee75
jialuyyy <janeyuan1992@gmail.com>
2017-06-04 12:18:33 -0400

    Update and rename partition.java to Partition.java

--- Partition.java
+++ Partition.java
@@ -19,7 +19,7 @@ return 1->2->2->4->3->5.*/
  *     ListNode(int x) { val = x; }
  * }
  */
-public class Solution {
+public class Partition {
     public ListNode partition(ListNode head, int x) {
         if (head == null) {
             return head;
