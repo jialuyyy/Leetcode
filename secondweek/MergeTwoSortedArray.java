d1332b7524389599fb36cf867c056c3d5b1119b7
jialuyyy <janeyuan1992@gmail.com>
2017-06-10 19:18:35 -0400

    Create MergeTwoSortedArray.java

---
+++ secondweek/MergeTwoSortedArray.java
@@ -0,0 +1,23 @@
+public class MergeTwoSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = nums1.length - 1;
+        int j = m - 1;
+        int k = n - 1;
+        
+        while (k >= 0 && j >= 0) {
+            if (nums1[j] > nums2[k]) {
+                nums1[i--] = nums1[j--];
+            } else {
+                nums1[i--] = nums2[k--];
+            }
+        }
+        
+        while (k >= 0) {
+            nums1[i--] = nums2[k--];
+        }
+        
+        while (j >= 0) {
+            nums1[i--] = nums1[j--];
+        }
+    }
+}
