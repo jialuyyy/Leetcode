048e0c246d704f74e35e8f2ec83072bdbd7ef195
jialuyyy <janeyuan1992@gmail.com>
2017-06-11 19:34:03 -0400

    Create SwapPairs.java

---
+++ secondweek/SwapPairs.java
@@ -0,0 +1,32 @@
+//beats 83.31%
+//iterate over the while linked list and keep two pointers to the first list node and second list node, every time 
+//update the current node
+//Time complexity: O(n)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class SwapPairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        
+        ListNode cur = dummy;
+        
+        while (cur.next != null && cur.next.next != null) {
+            ListNode first = cur.next;
+            ListNode second = cur.next.next;
+            
+            first.next = second.next;
+            second.next = first;
+            cur.next = second;
+            cur = cur.next.next;
+        }
+        
+        return dummy.next;
+    }
+}
