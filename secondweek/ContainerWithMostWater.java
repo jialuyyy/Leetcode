b18585c69a688a45096ef22220ea384ec66cbd5e
jialuyyy <janeyuan1992@gmail.com>
2017-06-11 10:50:18 -0400

    Create ContainerWithMostWater.java

--- secondweek/ContainerWithMostWater.java
+++ secondweek/ContainerWithMostWater.java
@@ -1,5 +1,8 @@
 //Time Complexity: O(n)
 //beats:12.00%
+//Area = (right - left) * Math.min(height(right) - height(left));
+//as the right and left pointer move closer, the (right-left) is decreasing, if we want to find the max value, we need to increase the
+//Math.min(height(right) - height(left)), so we need to move the smaller height toward the higher height.
 public class ContainerWithMostWater {
     public int maxArea(int[] height) {
         if (height == null || height.length <= 1) {
