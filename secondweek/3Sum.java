f4287f8e2df492aadc2d518af433a98f75012ea7
jialuyyy <janeyuan1992@gmail.com>
2017-06-11 09:53:55 -0400

    Create 3Sum.java

--- secondweek/3Sum.java
+++ secondweek/3Sum.java
@@ -1,3 +1,47 @@
+//brute force 311/313 passed
+//Time limit exceeded
+//Time Complexity: O(n ^ 3)
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length < 3) {
+            return ret;
+        }
+        
+        Arrays.sort(nums);
+        int i = 0;
+        
+        for (; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int j = i + 1;
+            for (; j < nums.length - 1; j++) {
+                int k = j + 1;
+                for (; k < nums.length; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(nums[i]);
+                        list.add(nums[j]);
+                        list.add(nums[k]);
+                        ret.add(list);
+                    }
+                    while (k < nums.length - 1 && nums[k] == nums[k + 1]) {
+                        k++;
+                    }
+                }
+                
+                
+                while (j < nums.length - 2 && nums[j] == nums[j + 1]) {
+                    j++;
+                }
+            }
+        }
+        
+        return ret;
+    }
+}
+
 //beats 19.53%
 //three pointers. i starts from the first element; j and k one starts from the element behind i and the other starts from the end
 //Time Complexity O(n ^ 2)
