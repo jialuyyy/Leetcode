d335018943945757f508ab38179a32b3d1da7a27
jialuyyy <janeyuan1992@gmail.com>
2017-06-12 09:12:58 -0400

    Create IsStrobigrammatic.java

--- secondweek/IsStrobigrammatic.java
+++ secondweek/IsStrobigrammatic.java
@@ -19,3 +19,42 @@ public class IsStrobigrammatic {
         
     }
 }
+
+
+//use a hashmap to store the mirror character pair
+//two pointers move toward each other to judge if this string is strobogrammatic
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+//beats 9.16%
+public class Solution {
+    public boolean isStrobogrammatic(String num) {
+        if (num == null) {
+            return false;
+        }
+        
+        Map<Character, Character> map = new HashMap<Character, Character>();
+        map.put('9', '6');
+        map.put('6', '9');
+        map.put('0', '0');
+        map.put('1', '1');
+        map.put('8', '8');
+        
+        int left = 0;
+        int right = num.length() - 1;
+        
+        while (left <= right) {
+            if (map.get(num.charAt(left)) == null) {
+                return false;
+            }
+            
+            if (map.get(num.charAt(left)) != num.charAt(right)) 
+                return false;
+            
+            left++;
+            right--;
+        }
+        
+        return true;
+        
+    }
+}
