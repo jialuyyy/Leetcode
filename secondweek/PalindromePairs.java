ccfb98a3ad85d9b24941841e227f21ef9d371a63
jialuyyy <janeyuan1992@gmail.com>
2017-06-13 12:47:00 -0400

    Create PalindromePairs.java

--- secondweek/PalindromePairs.java
+++ secondweek/PalindromePairs.java
@@ -2,7 +2,7 @@
 //O(kn ^ 2), n is the length of the words array and k is the average length of two strings length
 //Time limit exceeds, need to do optimization
 
-public class Solution {
+public class PalindromePairs {
     public List<List<Integer>> palindromePairs(String[] words) {
         List<List<Integer>> ret = new ArrayList<List<Integer>>();
         if (words == null || words.length == 0) {
@@ -30,6 +30,72 @@ public class Solution {
                         ret.add(list);
                     }
                 
+            }
+        }
+        
+        return ret;
+    }
+    
+    private boolean isPalindrome(String s) {
+        int start = 0;
+        int end = s.length() - 1;
+        
+        while (start < end) {
+            if (s.charAt(start) != s.charAt(end)) {
+                return false;
+            }
+            
+            start++;
+            end--;
+        }
+        
+        return true;
+    }
+}
+
+//Time Complexity: O(n * (k ^ 2)) k is the average length of each word and n is the length of the string array
+//Space Complexity: O(n), use hashmap to locate the word in the string array
+//thoughts: iterate over each word in the words array. split each word into two substrings, if one of the two substrings is a palindrome
+//then just need to locate if the words array contains a string that is the reverse of the other substring. If it is, we find one solution.
+//Compared with the brute force solution, this solution optimized the algorithm by decreasing one iteration.
+
+public class Solution {
+    public List<List<Integer>> palindromePairs(String[] words) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (words == null || words.length == 0) {
+            return ret;
+        }
+        
+        Map<String, Integer> map = new HashMap<String, Integer>();
+        for (int i = 0; i < words.length; i++) {
+            map.put(words[i], i);
+        }
+        
+        for (int i = 0; i < words.length; i++) {
+            
+            for (int j = 0; j <= words[i].length(); j++) {
+                String str1 = words[i].substring(0, j);
+                String str2 = words[i].substring(j);
+                
+                if (isPalindrome(str1)) {
+                    String target = new StringBuilder(str2).reverse().toString();
+                    if (map.get(target) != null && map.get(target) != i) {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(map.get(target));
+                        list.add(i);
+                        ret.add(list);
+                    }
+                }
+                
+                if (isPalindrome(str2)) {
+                    String target = new StringBuilder(str1).reverse().toString();
+                    if (map.get(target) != null && map.get(target) != i && str2.length() != 0) {
+                        List<Integer> list = new ArrayList<Integer>();
+                        list.add(i);
+                        list.add(map.get(target));
+                        ret.add(list);
+                    }
+                }
             }
         }
         
