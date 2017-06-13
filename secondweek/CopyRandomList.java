b27e3ea4012adb90b4fae593c3d16d6690589332
jialuyyy <janeyuan1992@gmail.com>
2017-06-13 09:38:41 -0400

    Create CopyRandomList.java

--- secondweek/CopyRandomList.java
+++ secondweek/CopyRandomList.java
@@ -46,3 +46,62 @@ public class CopyRandomList {
         return hashMap.get(head);
     }
 }
+
+
+
+//in place solution, no need to use hashmap
+//beats 72.16%
+//Time Complexity: O(n) three passes
+//Space Complexity: O(n)
+/**
+ * Definition for singly-linked list with a random pointer.
+ * class RandomListNode {
+ *     int label;
+ *     RandomListNode next, random;
+ *     RandomListNode(int x) { this.label = x; }
+ * };
+ */
+public class Solution {
+    public RandomListNode copyRandomList(RandomListNode head) {
+        if (head == null) {
+            return head;
+        }
+        RandomListNode tmp = head;
+        
+        //copy the label of each node and connect the copied node right after the original one
+        //the first pass
+        while (tmp != null) {
+            RandomListNode copy = new RandomListNode(tmp.label);
+            copy.next = tmp.next;
+            tmp.next = copy;
+            tmp = tmp.next.next;
+        }
+        
+        
+        tmp = head;
+        //the second pass
+        //copy random
+        while (tmp != null) {
+            if (tmp.random != null) {
+                tmp.next.random = tmp.random.next;
+            }
+            tmp = tmp.next.next;
+        }
+        
+        
+        tmp = head;
+        //third pass
+        //split the list
+        RandomListNode copyHead = tmp.next;
+        while (tmp != null) {
+            RandomListNode cur = tmp.next;
+            tmp.next = cur.next;
+            if (cur.next != null)
+                cur.next = cur.next.next;
+            
+            tmp = tmp.next;
+        }
+        
+        return copyHead;
+    }
+}
