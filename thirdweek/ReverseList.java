10868a04a00ea2b22ff8af8f98c05a4caca68d83
jialuyyy <janeyuan1992@gmail.com>
2017-06-17 16:59:53 -0400

    Create ReverseList.java

---
+++ thirdweek/ReverseList.java
@@ -0,0 +1,53 @@
+//non-tail recursion solution
+//beats 4.30%
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        
+        return newHead;
+    }
+}
+
+
+//beats 36.09%
+//non-recursive solution
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode newHead = null;
+        
+        while (head != null) {
+            ListNode tmp = head.next;
+            head.next = newHead;
+            newHead = head;
+            head = tmp;
+        }
+        
+        return newHead;
+    }
+}
