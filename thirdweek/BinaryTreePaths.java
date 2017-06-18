84058c0c0b24c9795fae5cfeb02c404e77a8a872
jialuyyy <janeyuan1992@gmail.com>
2017-06-17 20:28:13 -0400

    Update BinaryTreePaths.java

--- thirdweek/BinaryTreePaths.java
+++ thirdweek/BinaryTreePaths.java
@@ -33,3 +33,43 @@ public class BinaryTreePaths {
         helper(root.right, str + root.val + "->", ret);
     }
 }
+
+//use String builder need to reset the length of the string every time, beats 73.12%
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> ret = new ArrayList<String>();
+        if (root == null) {
+            return ret;
+        }
+        
+        helper(root, new StringBuilder(), ret);
+        return ret;
+    }
+    
+    private void helper(TreeNode root, StringBuilder sb, List<String> ret) {
+        if (root == null) {
+            return;
+        }
+        
+        int len = sb.length();
+        if (root.left == null && root.right == null) {
+            sb.append(root.val);
+            ret.add(sb.toString());
+            sb.setLength(len);
+            return;
+        }
+        sb.append(root.val + "->");
+        helper(root.left, sb, ret);
+        helper(root.right, sb, ret);
+        sb.setLength(len);
+    }
+}
