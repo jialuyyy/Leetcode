8bf399823ba69438c77155f178c68840c610b249
jialuyyy <janeyuan1992@gmail.com>
2017-06-19 10:11:51 -0400

    Update FindMedianSortedArrays.java

--- thirdweek/FindMedianSortedArrays.java
+++ thirdweek/FindMedianSortedArrays.java
@@ -32,3 +32,47 @@ public class FindMedianSortedArrays {
         }
     }
 }
+
+
+//find the kth element in two sorted arrays
+//k = (nums1.length + nums2.length) / 2;
+//beats 73.77%
+//Time complexity: O(log(n))
+public class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int k = nums1.length + nums2.length;
+        if (k % 2 == 0) {
+            return (findKthElement(k / 2, nums1, nums2, 0, 0) + findKthElement(k / 2 + 1, nums1, nums2, 0, 0)) / 2.0;
+        }
+        
+        return findKthElement(k / 2 + 1, nums1, nums2, 0, 0);
+    }
+    
+    private int findKthElement(int k, int[] nums1, int[] nums2, int h1, int h2) {
+        if (h1 >= nums1.length) {
+            return nums2[h2 + k - 1];
+        }
+        
+        if (h2 >= nums2.length) {
+            return nums1[h1 + k - 1];
+        }
+        
+        if (k == 1) {
+            return Math.min(nums1[h1], nums2[h2]);
+        }
+        
+        int m1 = h1 + k / 2 - 1;
+        int m2 = h2 + k / 2 - 1;
+        
+        int mid1 = m1 < nums1.length ? nums1[m1] : Integer.MAX_VALUE;
+        int mid2 = m2 < nums2.length ? nums2[m2] : Integer.MAX_VALUE;
+        
+        //find k-2/k element in the remaining elements
+        if (mid1 < mid2) {
+            return findKthElement(k - k / 2, nums1, nums2, m1 + 1, h2);
+        } else {
+            return findKthElement(k - k / 2, nums1, nums2, h1, m2 + 1);
+        }
+        
+    }
+}
