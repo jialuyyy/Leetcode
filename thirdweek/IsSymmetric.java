9af8b99459366377372894bcf27ef39b6bccfaea
jialuyyy <janeyuan1992@gmail.com>
2017-06-18 21:01:56 -0400

    Create IsSymmetric.java

---
+++ thirdweek/IsSymmetric.java
@@ -0,0 +1,31 @@
+//beats 24.53%
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class IsSymmetric {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        return helper(root.left, root.right);
+    }
+    
+    private boolean helper(TreeNode left, TreeNode right) {
+        if (left == null && right == null) {
+            return true;
+        }
+        
+        if (left == null || right == null) {
+            return false;
+        }
+        
+        return left.val == right.val && helper(left.left, right.right) && helper(left.right, right.left);
+    }
+}
