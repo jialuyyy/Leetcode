885d9bc9726e2d000b1adbaf233e71884558bb3e
jialuyyy <janeyuan1992@gmail.com>
2017-06-18 21:04:34 -0400

    Create SameTree.java

---
+++ thirdweek/SameTree.java
@@ -0,0 +1,22 @@
+//beats 24.50%
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class SameTree {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        
+        if (p == null || q == null)
+            return false;
+        
+        return p.val == q.val && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
