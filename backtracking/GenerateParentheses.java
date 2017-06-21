f7cff4b6d77f56b41f8ac7ba3aaaa8cf0a9c9a49
jialuyyy <janeyuan1992@gmail.com>
2017-06-20 20:37:47 -0400

    Create GenerateParentheses.java

---
+++ backtracking/GenerateParentheses.java
@@ -0,0 +1,24 @@
+public class GenerateParentheses {
+    public List<String>  generateParenthesis(int n) {
+        List<String> ret = new ArrayList<String>();
+        
+        helper(0, 0, "", n, ret);
+        return ret;
+    }
+    
+    
+    private void helper(int close, int open, String str, int max, List<String> ret) {
+        if (str.length() == max * 2) {
+            ret.add(str);
+            return;
+        }
+        
+        if (open < max) {
+            helper(close, open + 1, str + "(", max, ret);
+        }
+        
+        if (close < open) {
+            helper(close + 1, open, str + ")", max, ret);
+        }
+    }
+}
