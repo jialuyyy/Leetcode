4716fc4b812e7b59834a39bf83731b7a30a5cbb8
jialuyyy <janeyuan1992@gmail.com>
2017-06-26 09:57:05 -0400

    Update KthSmallestInSortedMatrix.java

--- Forthweek/KthSmallestInSortedMatrix.java
+++ Forthweek/KthSmallestInSortedMatrix.java
@@ -1,5 +1,5 @@
 //use a minheap to maintain the min value of the matrix. use a hash boolean two-dimensional matrix to avoid duplicates
-//Time Complexity: klog(k)
+//Time Complexity: klog(n)
 public class KthSmallestInSortedMatrix {
     private static int[] dx = {0, 1};
     private static int[] dy = {1, 0};
