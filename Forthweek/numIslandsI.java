b77b8572eefb2b98006ce840de7851b069bf5dd4
jialuyyy <janeyuan1992@gmail.com>
2017-06-25 21:02:28 -0400

    Update numIslandsI.java

--- Forthweek/numIslandsI.java
+++ Forthweek/numIslandsI.java
@@ -59,3 +59,37 @@ public class numIslandsI {
         
     }
 }
+
+
+//dfs 33.80%
+public class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    dfs(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+    private void dfs (char[][] grid, int x, int y) {
+        if (x < 0 || x >= grid.length || y < 0 || y >= grid[0].length || grid[x][y] != '1') {
+            return;
+        }
+        
+        grid[x][y] = '0';
+        dfs(grid, x + 1, y);
+        dfs(grid, x - 1, y);
+        dfs(grid, x, y - 1);
+        dfs(grid, x, y + 1);
+    }
+}
