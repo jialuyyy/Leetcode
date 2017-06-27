1baf804dd35c74170a0afbb9c029b3ff76f51531
jialuyyy <janeyuan1992@gmail.com>
2017-06-27 10:30:54 -0400

    Create WiggleSortI.java

---
+++ Forthweek/WiggleSortI.java
@@ -0,0 +1,21 @@
+public class WiggleSortI {
+    public void wiggleSort(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+        
+        for (int i = 1; i < nums.length; i++) {
+            if (i % 2 == 1 && nums[i] < nums[i - 1]) {
+                swap(nums, i, i - 1);
+            } else if (i % 2 == 0 && nums[i] > nums[i - 1]) {
+                swap(nums, i, i - 1);
+            }
+        }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+}
