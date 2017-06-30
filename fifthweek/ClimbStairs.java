731c94459d8f7560ba28e44d030dcc729c132fe3
jialuyyy <janeyuan1992@gmail.com>
2017-06-30 19:54:55 -0400

    Create ClimbStairs.java

---
+++ fifthweek/ClimbStairs.java
@@ -0,0 +1,15 @@
+//dp[i] = dp[i - 1] + dp[i - 2]
+//Time Complexity: O(n)
+public class ClimbStairs {
+    public int climbStairs(int n) {
+        int[] dp = new int[n + 1];
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        
+        return dp[n];
+    }
+}
