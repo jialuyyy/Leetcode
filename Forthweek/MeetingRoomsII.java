d2a3ef69ec0b4089bd4b73ca14a51d931d330c35
jialuyyy <janeyuan1992@gmail.com>
2017-06-30 09:19:35 -0400

    Update MeetingRoomsII.java

--- Forthweek/MeetingRoomsII.java
+++ Forthweek/MeetingRoomsII.java
@@ -57,3 +57,65 @@ public class MeetingRoomsII {
         return pq.size();
     }
 }
+
+
+//version 2, sweeping line solution
+//split intervals into two Pair, one is starting pair and the other one is ending pair
+//sort the pair based on the timestamp, if two timestamps are equal, put the ending one first
+//iterate over the whole pair list, if meeting start, count++; else, count--, use a ret variable to maintain the maximum result in the 
+//whole process and return it.
+/**
+ * Definition for an interval.
+ * public class Interval {
+ *     int start;
+ *     int end;
+ *     Interval() { start = 0; end = 0; }
+ *     Interval(int s, int e) { start = s; end = e; }
+ * }
+ */
+public class Solution {
+    class Pair {
+        int timeStamp;
+        int isStart;
+        
+        public Pair(int timeStamp, int isStart) {
+            this.timeStamp = timeStamp;
+            this.isStart = isStart;
+        }
+    }
+    public int minMeetingRooms(Interval[] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return 0;
+        }
+        
+        List<Pair> list = new ArrayList<Pair>();
+        for (Interval interval: intervals) {
+            list.add(new Pair(interval.start, 1));
+            list.add(new Pair(interval.end, 0));
+        }
+        
+        Collections.sort(list, new Comparator<Pair>(){
+            public int compare(Pair p1, Pair p2) {
+                if (p1.timeStamp == p2.timeStamp) {
+                    //end is 0 and start is 1, so end will be in the front of start
+                    return p1.isStart - p2.isStart;
+                }
+                return p1.timeStamp - p2.timeStamp;
+            }
+        });
+        
+        int minMeetingRoom = 0;
+        int count = 0;
+        for (Pair pair: list) {
+            if (pair.isStart == 1) {
+                count++;
+            } else {
+                count--;
+            }
+            
+            minMeetingRoom = Math.max(minMeetingRoom, count);
+        }
+        
+        return minMeetingRoom;
+    }
+}
