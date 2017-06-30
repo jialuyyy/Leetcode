6222ea3ae5dcafe07be34394eb93f69708f3dadd
jialuyyy <janeyuan1992@gmail.com>
2017-06-30 19:36:25 -0400

    Create LIS.java

---
+++ fifthweek/LIS.java
@@ -0,0 +1,25 @@
+public class LIS {
+    public int lengthOfLIS(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[nums.length];
+        for (int i = 0; i < dp.length; i++) {
+            dp[i] = 1;
+        }
+        int maxLen = 1;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] = Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            if (dp[i] > maxLen) {
+                maxLen = dp[i];
+            }
+        }
+        
+        return maxLen;
+    }
+}
