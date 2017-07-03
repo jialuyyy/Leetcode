e64d118c7b6bd6184cd1db7b117acb1743d44a9b
jialuyyy <janeyuan1992@gmail.com>
2017-07-02 21:44:27 -0400

    Update MaxProfitI.java

--- fb/MaxProfitI.java
+++ fb/MaxProfitI.java
@@ -22,3 +22,32 @@ public class MaxProfitI {
         return globalMax;
     }
 }
+
+
+//Time Complexity: O(n)
+//use a min pointer pointing to the minimum value of the array, if the next price value is larger than current minimum price, update
+//the profit if needed.
+//beats 55.56%
+
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) {
+            return 0;
+        }
+    
+        int profit = 0;
+        
+        int min = prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            
+            if (prices[i] < min) {
+                min = prices[i];
+            } else {
+                profit = Math.max(profit, prices[i] - min);
+            }
+            
+        }
+        
+        return profit;
+    }
+}
