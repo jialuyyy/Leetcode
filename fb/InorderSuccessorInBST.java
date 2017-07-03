33276794d09e16b9c73efdf18bc6c1cbf8d36efd
jialuyyy <janeyuan1992@gmail.com>
2017-07-02 20:44:28 -0400

    Update InorderSuccessorInBST.java

--- fb/InorderSuccessorInBST.java
+++ fb/InorderSuccessorInBST.java
@@ -44,3 +44,49 @@ public class InorderSuccessorInBST {
         helper(root.right, list);
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null;
+        }
+        
+        TreeNode successor = null;
+        while (root != null && root.val != p.val) {
+            if (root.val > p.val) {
+                successor = root;
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        
+        //no p
+        if (root == null) {
+            return null;
+        }
+        
+        //if root.right == null, return successor
+        if (root.right == null) {
+            return successor;
+        }
+        
+        //root.right != null, return the left most treenode of the right node
+        TreeNode cur = root.right;
+        while (cur.left != null) {
+            cur = cur.left;
+        }
+        
+        return cur;
+    }
+}
