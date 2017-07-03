48f78c057a27436e907879279c3ec6f7a4374a0f
jialuyyy <janeyuan1992@gmail.com>
2017-07-02 20:32:29 -0400

    Create InorderSuccessorInBST.java

---
+++ fb/InorderSuccessorInBST.java
@@ -0,0 +1,46 @@
+//primitive idea, use a list to keep the inorder traversal treenode of the given bst
+//locate the target treenode in the list and return the next one
+//Time complexity: O(n)
+//Space Complexity: O(n)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class InorderSuccessorInBST {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        if (root == null || p == null) {
+            return null;
+        }
+        
+        List<TreeNode> list = new ArrayList<TreeNode>();
+        helper(root, list);
+        
+        for (int i = 0; i < list.size(); i++) {
+            TreeNode node = list.get(i);
+            if (node.val == p.val) {
+                if (i == list.size() - 1) {
+                    return null;
+                } else {
+                    return list.get(i + 1);
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    private void helper(TreeNode root, List<TreeNode> list) {
+        if (root == null) {
+            return;
+        }
+        
+        helper(root.left, list);
+        list.add(root);
+        helper(root.right, list);
+    }
+}
