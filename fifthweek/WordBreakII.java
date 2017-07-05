24ae73f1c96e5bdbd56a0359a8e6720c7597b314
jialuyyy <janeyuan1992@gmail.com>
2017-07-05 10:15:58 -0400

    Update WordBreakII.java

--- fifthweek/WordBreakII.java
+++ fifthweek/WordBreakII.java
@@ -59,4 +59,77 @@ public class WordBreakII {
             temp.remove(temp.size() - 1);
         }
     }
+    
+    
+    // isWord maintains whether s.substring(i, j + 1) is in the dictionary
+    //isPossible maintains whether index from i to the end will be a possible perfect partition
+    //if isPossible[0] = false, which means there is no perfect partition of the input string
+    // if isPossible[0] is true, which means there exists perfect partition given the input string
+    //if the startIndex == s.length(), which means we already found one solution and put it int the result list
+    //else, iterate over from the startIndex, if found a word, iterate recursively until found a valid solution
+    
+    public class WordBreakII {
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        List<String> ret = new ArrayList<String>();
+        if (s == null || wordDict == null) {
+            return ret;
+        }
+        
+        boolean[][] isWord = new boolean[s.length()][s.length()];
+        
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                if (wordDict.contains(s.substring(i, j + 1))) {
+                    isWord[i][j] = true;
+                }
+            }
+        }
+        
+        boolean[] isPossible = new boolean[s.length() + 1];
+        isPossible[s.length()] = true;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (isWord[i][j] && isPossible[j + 1]) {
+                    isPossible[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        search(isWord, isPossible, 0, new ArrayList<Integer>(), ret, s);
+        return ret;
+    }
+    
+    private void search(boolean[][] isWord, boolean[] isPossible, int startIndex, List<Integer> path, List<String> ret, String s) {
+        if (!isPossible[startIndex]) {
+            return;
+        }
+        
+        if (startIndex == s.length()) {
+            StringBuilder sb = new StringBuilder();
+            int lastIndex = 0;
+            for (int i = 0; i < path.size(); i++) {
+                sb.append(s.substring(lastIndex, path.get(i)));
+                if (i != path.size() - 1) {
+                    sb.append(" ");
+                }
+                lastIndex = path.get(i);
+            }
+            
+            ret.add(sb.toString());
+        }
+        
+        for (int i = startIndex; i < s.length(); i++) {
+            if (!isWord[startIndex][i]) {
+                continue;
+            }
+            
+            path.add(i + 1);
+            search(isWord, isPossible, i + 1, path, ret, s);
+            path.remove(path.size() - 1);
+        }
+        
+    }
+}
 }
