c265060e13d172aaac3ed0d3922f52b2dfcd4ad1
jialuyyy <janeyuan1992@gmail.com>
2017-07-05 09:39:58 -0400

    Update ClimbStairs.java

--- fifthweek/ClimbStairs.java
+++ fifthweek/ClimbStairs.java
@@ -13,3 +13,26 @@ public class ClimbStairs {
         return dp[n];
     }
 }
+
+//iterative solution
+public class Solution {
+    public int climbStairs(int n) {
+        if (n <= 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
+        
+        int oneStepBefore = 2;
+        int twoStepBefore = 1;
+        int allWays = 0;
+        for (int i = 2; i < n; i++) {
+            allWays = oneStepBefore + twoStepBefore;
+            twoStepBefore = oneStepBefore;
+            oneStepBefore = allWays;
+        }
+        
+        return allWays;
+    }
+}
