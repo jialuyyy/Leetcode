ac0fbb9265096497d74b86c9978ce70bc4e34e57
jialuyyy <janeyuan1992@gmail.com>
2017-07-05 09:29:43 -0400

    Update PaintHouseII.java

--- fifthweek/PaintHouseII.java
+++ fifthweek/PaintHouseII.java
@@ -38,3 +38,49 @@ public class PaintHouseII {
         return ret;
     }
 }
+
+
+//optimized solution: two for loops 
+//Time Complexity: O(n ^ 2)
+
+public class Solution {
+    public int minCostII(int[][] costs) {
+        if (costs == null || costs.length == 0) {
+            return 0;
+        }
+        
+        int houseNumber = costs.length;
+        int colorNumber = costs[0].length;
+        
+        int min1 = -1;
+        int min2 = -1;
+        
+        for (int i = 0; i < houseNumber; i++) {
+            //keep the last 1st and 2nd minimum elements' index
+            int lastMin1 = min1;
+            int lastMin2 = min2;
+                
+            min1 = -1;
+            min2 = -1;
+            
+            for (int j = 0; j < colorNumber; j++) {
+                if (j != lastMin1) {
+                    costs[i][j] += lastMin1 < 0 ? 0 : costs[i - 1][lastMin1];
+                } else {
+                    costs[i][j] += lastMin2 < 0 ? 0 : costs[i - 1][lastMin2];
+                }
+                
+                //update the index of the 1st and 2nd minimum elements
+                if (min1 < 0 || costs[i][j] < costs[i][min1]) {
+                    min2 = min1; 
+                    min1 = j;
+                } else if (min2 < 0 || costs[i][j] < costs[i][min2]) {
+                    min2 = j;
+                }
+            }
+        }
+        
+        
+        return costs[costs.length - 1][min1];
+    }
+}
