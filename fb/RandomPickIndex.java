56803adba49619bf5aacf85bda08177ec5a76e9e
jialuyyy <janeyuan1992@gmail.com>
2017-07-06 09:50:10 -0400

    Update RandomPickIndex.java

--- fb/RandomPickIndex.java
+++ fb/RandomPickIndex.java
@@ -29,3 +29,39 @@ public class RandomPickIndex {
  * Solution obj = new Solution(nums);
  * int param_1 = obj.pick(target);
  */
+
+//optimized solution, no need to use the hasmap to store the index of every element appearing in the array.
+//use rand.nextInt(current total number of the target number) to get the random index from 0 to total, and total is excluded in the range
+//so for the latest appearing element, the probability of selected it is 1/total, for the previous element, the total probability of 
+//selecting them is 1 - 1/total and for each of them, the probability should be (1 - 1/total) * 1/(total - 1) is also 1/total.
+//so there is no need to use extra space.
+
+public class RandomPickIndex {
+    int[] nums;
+    Random rand;
+    public Solution(int[] nums) {
+        this.nums = nums;
+        this.rand = new Random();
+    }
+    
+    public int pick(int target) {
+        int total = 0;
+        int res = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                //get an int in the range of [0, i), so for the latest appearing element, the probability for it to get selected
+                //is 1/total, and for every previous element, the probability is equal
+                int nextInt = rand.nextInt(++total);
+                res = nextInt == 0 ? i : res;
+            }
+        }
+        
+        return res;
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int param_1 = obj.pick(target);
+ */
