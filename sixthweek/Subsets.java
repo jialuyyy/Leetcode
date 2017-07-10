094bfd654c18fa51e0736ed4ab1aac8ab39dff83
Jane Yuan <janeyuan1992@gmail.com>
2017-07-10 10:32:10 -0400

    Create Subsets.java

---
+++ sixthweek/Subsets.java
@@ -0,0 +1,22 @@
+//beats 28%
+public class Subsets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (nums == null) {
+            return ret;
+        }
+        
+        helper(nums, 0, new ArrayList<Integer>(), ret);
+        return ret;
+    }
+    
+    private void helper(int[] nums, int pos, List<Integer> list, List<List<Integer>> ret) {
+        ret.add(new ArrayList<Integer>(list));
+        
+        for (int i = pos; i < nums.length; i++) {
+            list.add(nums[i]);
+            helper(nums, i + 1, list, ret);
+            list.remove(list.size() - 1);
+        }
+    }
+}
