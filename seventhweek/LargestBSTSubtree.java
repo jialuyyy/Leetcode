e1c9be6db45c02e62d9cd3fd7fd17e115d06b82e
Jane Yuan <janeyuan1992@gmail.com>
2017-07-13 11:00:30 -0400

    Update LargestBSTSubtree.java

--- seventhweek/LargestBSTSubtree.java
+++ seventhweek/LargestBSTSubtree.java
@@ -43,3 +43,60 @@ public class LargestBSTSubtree {
         return count(root.left) + count(root.right) + 1;
     }
 }
+
+//use a wrapper class to do bottom up
+//size is the current count for current node, max is the max value of the current subtree and min is the minimum value of the current
+//subtree
+//when the node == null, the maxValue should be the Integer.MIN_VALUE and the minValue should be the Integer.MAX_VALUE,
+//which will ensure that the root.val is in the range. if the left or the right subtree is not a BST, return ResultType(-1, 0, 0)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    class ResultType {
+        int size;
+        int max;
+        int min;
+        
+        public ResultType(int size, int max, int min) {
+            this.size = size;
+            this.max = max;
+            this.min = min;
+        }
+    }
+    public int largestBSTSubtree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        
+        int[] max = new int[1];
+        helper(root, max);
+        
+        return max[0];
+    }
+    
+    private ResultType helper(TreeNode root, int[] max) {
+        if (root == null) {
+            return new ResultType(0, Integer.MIN_VALUE, Integer.MAX_VALUE);
+        }
+        
+        ResultType left = helper(root.left, max);
+        ResultType right = helper(root.right, max);
+        
+        
+        if (left.size == -1 || right.size == -1 || left.max >= root.val || right.min <= root.val) {
+            return new ResultType(-1, 0, 0);
+        } else {
+            max[0] = Math.max(left.size + right.size + 1, max[0]);
+            return new ResultType(left.size + right.size + 1, Math.max(root.val, right.max), Math.min(root.val, left.min));
+        }
+    }
+    
+    
+}
