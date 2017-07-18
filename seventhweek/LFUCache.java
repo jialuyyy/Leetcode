c06a1013d3651d985a3aa7c246bb0e5fc12c1e9b
Jane Yuan <janeyuan1992@gmail.com>
2017-07-18 12:40:06 -0400

    Update LFUCache.java

--- seventhweek/LFUCache.java
+++ seventhweek/LFUCache.java
@@ -114,3 +114,106 @@ public class LFUCache {
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
+
+//TreeSet use compareTo to compare the values in it, so need to use a hashmap that key-value pair is key and cachenode to retrieve the 
+//node in treenode
+//the remove time complexity here is O(log(n)) n is the capacity of the map
+public class LFUCache {
+    private int capacity;
+    private TreeSet<CacheNode> treeSet = null;
+    //maintain the key-value pair
+    private Map<Integer, Integer> keyValueMap = null;
+    //maintain the key-cacheNode pair
+    private Map<Integer, CacheNode> cacheMap = null;
+    private int id = 0;
+    class CacheNode implements Comparable<CacheNode>{
+        int key;
+        int freq;
+        int recentness;
+        
+        public CacheNode(int key, int freq, int recentness) {
+            this.key = key;
+            this.freq = freq;
+            this.recentness = recentness;
+        }
+        
+        //override the equals() and hashCode() method, to do the remove in the priority queue
+        
+        public int hashCode() {return key;}
+        public int compareTo(CacheNode o) {return key == o.key? 0 : freq == o.freq ? recentness - o.recentness:freq - o.freq;}
+        
+    }
+    public LFUCache(int capacity) {
+        this.capacity = capacity;
+        
+        //treeset implements compareTo methods
+        this.treeSet = new TreeSet<CacheNode>();
+        
+        //initialize two hashmaps
+        this.keyValueMap = new HashMap<Integer, Integer>();
+        this.cacheMap = new HashMap<Integer, CacheNode>();
+    }
+    
+    //if the keyValueMap contains key
+    //update the frequency and the recentness and reput it in the priority queue
+    //else, return -1
+    public int get(int key) {
+        id++;
+        if (this.keyValueMap.containsKey(key)) {
+            update(key);
+            
+            return this.keyValueMap.get(key);
+        }
+        
+        return -1;
+    }
+    
+    //if the keyValueMap contains the key, update the current keyValueMap and update the frequencyMap and the queue
+    //if reaches the capacity, remove the least frequently used one from all of the three data structures
+    //create a new CacheNode and insert it into the priorityQueue, and add the key-value pair into the frequency map and the 
+    //keyValueMap
+    public void put(int key, int value) {
+        if (this.capacity == 0) {
+            return;
+        }
+        id++;
+        if (this.keyValueMap.containsKey(key)) {
+            update(key);
+            keyValueMap.put(key, value);
+            
+            return;
+        }
+        
+        if (keyValueMap.size() == this.capacity) {
+            CacheNode cur = treeSet.pollFirst();
+            keyValueMap.remove(cur.key);
+            cacheMap.remove(cur.key);
+        }
+        
+        
+        keyValueMap.put(key, value);
+        CacheNode node = new CacheNode(key, 1, id);
+        cacheMap.put(key, node);
+        treeSet.add(node);
+    }
+    
+    private void update(int key) {
+        //update the frequency
+        int f = cacheMap.get(key).freq + 1;
+        treeSet.remove(cacheMap.get(key));
+        
+        //create a new cache node with the new frequency and the recentness, the larger the id is, the larger the recentness
+        CacheNode cache = new CacheNode(key, f, id);
+        cacheMap.put(key, cache);
+        //remove the current cache node in the queue and offer the new node
+        treeSet.add(cache);
+        
+    }
+}
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache obj = new LFUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
