51103802fe4efdac766f6c98932fbd5f85eca79b
Jane Yuan <janeyuan1992@gmail.com>
2017-07-18 11:54:58 -0400

    Update Logger.java

--- seventhweek/Logger.java
+++ seventhweek/Logger.java
@@ -33,3 +33,57 @@ public class Logger {
  * Logger obj = new Logger();
  * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
  */
+
+
+//space saving solution
+//in this way, only the latest 10 seconds messages will be kept in the queue and hashset
+
+public class Logger {
+
+    /** Initialize your data structure here. */
+    Deque<Pair> q = null;
+    Set<String> set = null;
+    public Logger() {
+        q = new ArrayDeque<Pair>();
+        set = new HashSet<String>();
+    }
+    
+    /** Returns true if the message should be printed in the given timestamp, otherwise returns false.
+        If this method returns false, the message will not be printed.
+        The timestamp is in seconds granularity. */
+    
+    //only keep the latest 10 seconds messages in the queue to save the space
+    //if only use hashmap, the size of the hashmap will keep growing
+    
+    public boolean shouldPrintMessage(int timestamp, String message) {
+        while (!q.isEmpty() && q.peek().timestamp <= timestamp - 10) {
+            Pair cur = q.poll();
+            set.remove(cur.message);
+        }
+        
+        if (!set.contains(message)) {
+            set.add(message);
+            q.offer(new Pair(message, timestamp));
+            
+            return true;
+        }
+        
+        return false;
+    }
+    
+    class Pair {
+        String message;
+        int timestamp;
+        
+        public Pair (String message, int timestamp) {
+            this.message = message;
+            this.timestamp = timestamp;
+        }
+    }
+}
+
+/**
+ * Your Logger object will be instantiated and called as such:
+ * Logger obj = new Logger();
+ * boolean param_1 = obj.shouldPrintMessage(timestamp,message);
+ */
