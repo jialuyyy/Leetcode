fc5db31df5b8cea59aa946a31b201149f79adfb2
Jane Yuan <janeyuan1992@gmail.com>
2017-07-19 17:35:38 -0400

    Create LengthOfLongestSubstringTwoDistinct.java

---
+++ BloomBurg/LengthOfLongestSubstringTwoDistinct.java
@@ -0,0 +1,35 @@
+public class LengthOfLongestSubstringTwoDistinct {
+    public int lengthOfLongestSubstringTwoDistinct(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        int j = 0;
+        int maxLen = Integer.MIN_VALUE;
+        for (int i = 0; i < s.length(); i++) {
+            while (j < s.length()) {
+                if (map.containsKey(s.charAt(j))) {
+                    map.put(s.charAt(j), map.get(s.charAt(j)) + 1);
+                    j++;
+                } else {
+                    if (map.size() < 2) {
+                        map.put(s.charAt(j), 1);
+                        j++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+            
+            maxLen = Math.max(maxLen, j - i);
+            
+            if (map.get(s.charAt(i)) == 1)
+                map.remove(s.charAt(i));
+            else
+                map.put(s.charAt(i), map.get(s.charAt(i)) - 1);
+        }
+        
+        return maxLen;
+    }
+}
