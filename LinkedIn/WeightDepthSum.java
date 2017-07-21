2f32b6e78b737b4de1863878c92de404f2ff3354
Jane Yuan <janeyuan1992@gmail.com>
2017-07-21 10:04:27 -0400

    Create WeightDepthSum.java

---
+++ LinkedIn/WeightDepthSum.java
@@ -0,0 +1,47 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds, if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds, if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class WeightDepthSum {
+    private int depthSum = 0;
+    public int depthSum(List<NestedInteger> nestedList) {
+        if (nestedList == null || nestedList.size() == 0) {
+            return 0;
+        }
+        
+        for (int i = 0; i < nestedList.size(); i++) {
+            NestedInteger cur = nestedList.get(i);
+            dfs(cur, 1);
+        }
+        
+
+        return depthSum;
+    }
+    
+    private void dfs(NestedInteger nestedInteger, int depth) {
+        if (nestedInteger.isInteger()) {
+            depthSum += nestedInteger.getInteger() * depth;
+            return;
+        }
+        
+        List<NestedInteger> cur = nestedInteger.getList();
+        
+        for (int i = 0; i < cur.size(); i++) {
+            NestedInteger c = cur.get(i);
+            dfs(c, 1 + depth);
+        }
+    }
+}
