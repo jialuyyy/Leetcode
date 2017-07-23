9c0b1d82a4bdec3b58983326f1c035ff77ac648d
Jane Yuan <janeyuan1992@gmail.com>
2017-07-23 18:21:39 -0400

    Update kthDiffPairs.java

--- eighthweek/kthDiffPairs.java
+++ eighthweek/kthDiffPairs.java
@@ -30,3 +30,39 @@ public class kthDiffPairs {
         return count;
     }
 }
+
+//use a hashmap to avoid sorting
+//for the special case k == 0, need to check whether the array contains more than once of the same value
+//Time Complexity: O(n)
+//Space Complexity : O(n)
+public class Solution {
+    public int findPairs(int[] nums, int k) {
+        if (nums == null || nums.length <= 1 || k < 0) {
+            return 0;
+        }
+        
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int num: nums) {
+            if (map.get(num) == null) {
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+        }
+        
+        int count = 0;
+        for (Map.Entry<Integer, Integer> entry: map.entrySet()) {
+            if (k != 0) {
+                if (map.containsKey(entry.getKey() - k)) {
+                    count++;
+                }
+            } else {
+                if (entry.getValue() >= 2) {
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
