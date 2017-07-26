f35416ceb2b8dcfe1a80c57686934c7d9d760dbc
Jane Yuan <janeyuan1992@gmail.com>
2017-07-26 17:13:54 -0400

    Update WordDistance.java

--- LinkedIn/WordDistance.java
+++ LinkedIn/WordDistance.java
@@ -40,3 +40,47 @@ public class WordDistance {
  * WordDistance obj = new WordDistance(words);
  * int param_1 = obj.shortest(word1,word2);
  */
+
+
+public class WordDistance {
+    private Map<String, List<Integer>> map = null;
+    public WordDistance(String[] words) {
+        map = new HashMap<String, List<Integer>>();
+        for (int i = 0; i < words.length; i++) {
+            if (map.get(words[i]) == null) {
+                List<Integer> list = new ArrayList<Integer>();
+                list.add(i);
+                map.put(words[i], list);
+            } else {
+                map.get(words[i]).add(i);
+            }
+        }
+    }
+    
+    public int shortest(String word1, String word2) {
+        List<Integer> list1 = map.get(word1);
+        List<Integer> list2 = map.get(word2);
+        int min = Integer.MAX_VALUE;
+        if (list1.size() == 1 && list2.size() == 1) {
+            return Math.abs(list1.get(0) - list2.get(0));
+        } else {
+            for (int i = 0, j = 0; i < list1.size() && j < list2.size();) {
+                if (list1.get(i) < list2.get(j)) {
+                    min = Math.min(min, list2.get(j) - list1.get(i));
+                    i++;
+                } else {
+                    min = Math.min(min, list1.get(i) - list2.get(j));
+                    j++;
+                }
+            }
+        }
+        
+        return min;
+    }
+}
+
+/**
+ * Your WordDistance object will be instantiated and called as such:
+ * WordDistance obj = new WordDistance(words);
+ * int param_1 = obj.shortest(word1,word2);
+ */
