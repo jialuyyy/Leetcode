2073517b034491e0c61a22949206676db2929041
Jane Yuan <janeyuan1992@gmail.com>
2017-07-26 17:48:42 -0400

    Create UpsideDownBinaryTree.java

---
+++ LinkedIn/UpsideDownBinaryTree.java
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class UpsideDownBinaryTree {
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if (root == null || root.left == null) {
+            return root;
+        }
+        TreeNode newRoot = upsideDownBinaryTree(root.left);
+        
+        root.left.left = root.right;
+        root.left.right = root;
+        
+        root.left = null;
+        root.right = null;
+        
+        return newRoot;
+        
+    }
+}
