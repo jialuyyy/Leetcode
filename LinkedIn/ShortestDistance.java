2aeea6e9a90985822eb8a789de9c19f34ad9cd9d
Jane Yuan <janeyuan1992@gmail.com>
2017-07-26 19:13:44 -0400

    Update ShortestDistance.java

--- LinkedIn/ShortestDistance.java
+++ LinkedIn/ShortestDistance.java
@@ -70,3 +70,26 @@ public class Solution {
         return min;
     }
 }
+
+
+//more concise one
+public class Solution {
+    public int shortestDistance(String[] words, String word1, String word2) {
+        if (words == null || words.length == 0 || word1 == null || word2 == null) {
+            return 0;
+        }
+        
+        int min = Integer.MAX_VALUE;
+        int index = -1;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(word1) || words[i].equals(word2)) {
+                if (index != -1 && !words[index].equals(words[i])) {
+                    min = Math.min(min, i - index);
+                }
+                index = i;
+            }
+        }
+        
+        return min;
+    }
+}
