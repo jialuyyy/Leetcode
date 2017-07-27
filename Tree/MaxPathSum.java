8e23782c0945d8df4f0c08081820406a6a8e18a6
Jane Yuan <janeyuan1992@gmail.com>
2017-07-27 19:18:02 -0400

    Update MaxPathSum.java

--- Tree/MaxPathSum.java
+++ Tree/MaxPathSum.java
@@ -45,3 +45,65 @@ public class MaxPathSum {
         return Math.max(left, right) + root.val;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+/* use a wrapper class to record the single path sum and the max path sum of current node
+   single path sum means the max sum right from this node straight down and the max sum means the maxsum going through this node
+ */
+
+//divide and conqure
+//if the root == null, then the single sum should be 0 and the max sum should be Integer.MIN_VALUE
+// if the root != null, then the single sum should be the Math.max(left, right) + root.val, and if the value is negative, we should set
+//it to 0. for the max sum of current node, it should be the max sum of its left node, max sum of its right node, or left.singleSum + right.singleSum + root.val
+//the maximum of these three
+
+
+
+public class Solution {
+    class ResultType {
+        int singlePath = 0;
+        int maxPath = 0;
+        
+        //single path is from current node to go straight down and the maximum value
+        //max path is the maximum sum through current node
+        public ResultType(int singlePath, int maxPath) {
+            this.singlePath = singlePath;
+            this.maxPath = maxPath;
+        }
+    }
+    public int maxPathSum(TreeNode root) {
+        
+        ResultType rt = helper(root);
+        return rt.maxPath;
+    }
+    
+    private ResultType helper(TreeNode root) {
+        //if root == null
+        //single path sum == 0;
+        // max path sum == Integer.MIN_VALUE
+        if (root == null) {
+            return new ResultType(0, Integer.MIN_VALUE);
+        }
+        
+        ResultType left = helper(root.left);
+        ResultType right = helper(root.right);
+        
+        int singlePath = Math.max(left.singlePath, right.singlePath) + root.val;
+        singlePath = Math.max(0, singlePath);
+        
+        int maxPath = Math.max(left.maxPath, right.maxPath);
+        maxPath= Math.max(maxPath, left.singlePath + right.singlePath + root.val);
+        
+        return new ResultType(singlePath, maxPath);
+        
+    }
+}
