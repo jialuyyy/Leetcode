1033a385150dab66f4dd3a1855c863bbcf212157
Jane Yuan <janeyuan1992@gmail.com>
2017-07-28 13:34:57 -0400

    Update Flatten.java

--- Tree/Flatten.java
+++ Tree/Flatten.java
@@ -34,3 +34,30 @@ public class Flatten {
         
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    private TreeNode prev = null;
+    public void flatten(TreeNode root) {
+       if (root == null) {
+           return;
+       }
+        
+        flatten(root.right);
+        flatten(root.left);
+        
+        root.left = null;
+        root.right = prev;
+        
+        prev = root;
+    }
+}
