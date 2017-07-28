ce1987356612e885297a85a735d861949cca0fe7
Jane Yuan <janeyuan1992@gmail.com>
2017-07-28 09:30:33 -0400

    Update HasPathSum.java

--- Tree/HasPathSum.java
+++ Tree/HasPathSum.java
@@ -32,3 +32,28 @@ public class HasPathSum {
         return left || right;
     }
 }
+
+
+//more concise solution
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) {
+            return false;
+        }
+        
+        if (root.val == sum && root.left == null && root.right == null) {
+            return true;
+        }
+        
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
