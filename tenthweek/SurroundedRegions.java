65b66ec9128e7fac696d5fef1a8f9539058d2a0e
Jane Yuan <janeyuan1992@gmail.com>
2017-08-06 10:13:34 -0400

    Update SurroundedRegions.java

--- tenthweek/SurroundedRegions.java
+++ tenthweek/SurroundedRegions.java
@@ -73,3 +73,76 @@ public class SurroundedRegions {
         return true;
     }
 }
+
+//the 'O' that cannot be flipped to 'X' is connected to the boundary
+public class Solution {
+    
+    class Point {
+        int x;
+        int y;
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    
+    private int[] dx = {0, 0, -1, 1};
+    private int[] dy = {-1, 1, 0, 0};
+    public void solve(char[][] board) {
+        if (board == null || board.length == 0) {
+            return;
+        }
+        
+        int rows = board.length;
+        int cols = board[0].length;
+        
+        for (int i = 0; i < cols; i++) {
+            dfs(board, 0, i);
+            dfs(board, rows - 1, i);
+        }
+        
+        for (int i = 0; i < rows; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, cols - 1);
+        }
+        
+        for (int i = 0; i < rows; i++) {
+            for( int j = 0; j < cols; j++) {
+                if (board[i][j] == 'O') 
+                    board[i][j] = 'X';
+                
+                if (board[i][j] == '1')
+                    board[i][j] = 'O';
+                
+            }
+        }
+    }
+    
+    private void dfs (char[][] board, int i, int j) {
+        if (!isValid(i, j, board)) {
+            return;
+        }
+        
+        board[i][j] = '1';
+        if (i < board.length - 2)
+            dfs(board, i + 1, j);
+        
+        if (i > 1)
+            dfs(board, i - 1, j);
+        
+        if (j < board[0].length - 2)
+            dfs(board, i, j + 1);
+        
+        if (j > 1)
+        dfs(board, i, j - 1);
+        
+    }
+    
+    private boolean isValid(int i, int j, char[][] board) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != 'O') {
+            return false;
+        }
+        
+        return true;
+    }
+}
