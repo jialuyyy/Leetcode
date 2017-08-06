d814a0805bb1071317bc76c0c46dd5aacc00039e
Jane Yuan <janeyuan1992@gmail.com>
2017-08-06 17:57:41 -0400

    Update CountComponents.java

--- tenthweek/CountComponents.java
+++ tenthweek/CountComponents.java
@@ -1,3 +1,4 @@
+//Union Find
 public class CountComponents {
     class UF {
         int[] father = null;
@@ -49,5 +50,52 @@ public class CountComponents {
         }
         
         return total;
+    }
+}
+
+
+//Breadth-first traversal
+public class Solution {
+    public int countComponents(int n, int[][] edges) {
+        Map<Integer, Set<Integer>> map = new HashMap<Integer, Set<Integer>>();
+        boolean[] visited = new boolean[n];
+        for (int i = 0; i < n; i++) {
+            map.put(i, new HashSet<Integer>());
+        }
+        int count = 0;
+        //build graph
+        for (int[] edge: edges) {
+            int point1 = edge[0];
+            int point2 = edge[1];
+            
+            map.get(point1).add(point2);
+            map.get(point2).add(point1);
+        }
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(visited, map, i);
+                count++;
+            }
+        }
+        
+        return count;
+    }
+    
+    private void bfs (boolean[] visited, Map<Integer, Set<Integer>> map, int vertex) {
+        Deque<Integer> q = new ArrayDeque<Integer>();
+        q.offer(vertex);
+        visited[vertex] = true;
+        
+        while(!q.isEmpty()) {
+            int cur = q.poll();
+            
+            for (int neighbor: map.get(cur)) {
+                if (!visited[neighbor]) {
+                    q.offer(neighbor);
+                    visited[neighbor] = true;
+                }
+            }
+        }
     }
 }
