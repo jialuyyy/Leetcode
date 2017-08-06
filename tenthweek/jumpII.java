18f116916f47df8571fdf0fc66bdf87ac95fd2c2
Jane Yuan <janeyuan1992@gmail.com>
2017-08-06 18:51:54 -0400

    Update jumpII.java

--- tenthweek/jumpII.java
+++ tenthweek/jumpII.java
@@ -19,3 +19,29 @@ public class jumpII {
         return dp[nums.length - 1];
     }
 }
+
+
+//prune the starting points that are not able to reach the destination to reduce time complexity
+//find the first point that nums[j] + j >= i as the starting point for every i afterwards
+public class Solution {
+    public int jump(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[nums.length];
+        dp[0] = 0;
+        int curIndex = 0;
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = curIndex; j < i; j++) {
+                if (nums[j]  + j >= i) {
+                    dp[i] = dp[j] + 1;
+                    curIndex = j;
+                    break;
+                }
+            }
+        }
+        
+        return dp[nums.length - 1];
+    }
+}
