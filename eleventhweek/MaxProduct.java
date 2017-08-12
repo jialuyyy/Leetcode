2beefdb5b282fc0928f2d6b91a00b791791ee84a
Jane Yuan <janeyuan1992@gmail.com>
2017-08-12 10:02:32 -0400

    Create MaxProduct.java

---
+++ eleventhweek/MaxProduct.java
@@ -0,0 +1,21 @@
+public class MaxProduct {
+    public int maxProduct(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        
+        int max = nums[0];
+        int min = nums[0];
+        int globalMax = max;
+        
+        for (int i = 1; i < nums.length; i++) {
+            int tmp = max;
+            max = Math.max(Math.max(max * nums[i], min * nums[i]), nums[i]);
+            min = Math.min(Math.min(tmp * nums[i], min * nums[i]), nums[i]);
+            
+            globalMax = Math.max(globalMax, max);
+        }
+        
+        return globalMax;
+    }
+}
