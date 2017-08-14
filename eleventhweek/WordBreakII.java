c3135cec2130968293057dde8b003de5d359a612
Jane Yuan <janeyuan1992@gmail.com>
2017-08-14 08:23:34 -0400

    Update WordBreakII.java

--- eleventhweek/WordBreakII.java
+++ eleventhweek/WordBreakII.java
@@ -36,3 +36,29 @@ public class WordBreakII {
         
     }
 }
+
+
+//use a hashmap to avoid time limit exceeds
+public class Solution {
+    private Map<String, List<String>> map = new HashMap<String, List<String>>();
+    
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        if (map.containsKey(s))
+		    return map.get(s);
+
+	    LinkedList<String> res = new LinkedList<String>();
+	    if (s.length() == 0) {
+		    res.add("");
+		    return res;
+	    }
+	    for (String word : wordDict) {
+		    if (s.startsWith(word)) {
+			    List<String> sublist = wordBreak(s.substring(word.length()), wordDict);
+			    for (String sub : sublist)
+				    res.add(word + (sub.isEmpty() ? "" : " ") + sub);
+		    }
+	    }
+	    map.put(s, res);
+	    return res;
+    }
+}
