48a1fddeded59555ca15234982274976092f0528
Jane Yuan <janeyuan1992@gmail.com>
2017-08-14 21:03:34 -0400

    Create WildCard.java

---
+++ eleventhweek/WildCard.java
@@ -0,0 +1,32 @@
+//"aaabca"
+//"aaabca*"
+//dp[i][j] = dp[i][j - 1]
+
+//"aaabca"
+//"aaabc*"
+//dp[i][j] = dp[i - 1][j]
+
+public class WildCard {
+    public boolean isMatch(String s, String p) {
+        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
+        
+        dp[0][0] = true;
+        
+        for (int i = 1; i <= p.length(); i++) {
+            if (p.charAt(i - 1) == '*')
+                dp[0][i] = dp[0][i - 1];
+        }
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= p.length(); j++) {
+                if (p.charAt(j - 1) != '*') {
+                    dp[i][j] = dp[i - 1][j - 1] && (p.charAt(j - 1) == '?' || s.charAt(i - 1) == p.charAt(j - 1));
+                } else {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+            }
+        }
+        
+        return dp[s.length()][p.length()];
+    }
+    
+}
