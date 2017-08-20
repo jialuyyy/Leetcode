b7af36260a9970044f210f4e642408ecf2aef660
Jane Yuan <janeyuan1992@gmail.com>
2017-08-20 16:22:45 -0400

    Create BestMeetingPoint.java

---
+++ 12thweek/BestMeetingPoint.java
@@ -0,0 +1,71 @@
+//brute force bfs
+//time limit exceed for the last test case
+//because all the points are likely to be the meeting point, different than the walls and gate and the shortest distance to buildings
+class BestMeetingPoint {
+    private int[] dx = {0, 0, -1, 1};
+    private int[] dy = {-1, 1, 0, 0};
+    private int rows;
+    private int cols;
+    
+    class Point{
+        int x;
+        int y;
+        
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public int minTotalDistance(int[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        int[][] dist = new int[this.rows][this.cols];
+        
+        for (int i = 0; i < this.rows; i++) {
+            for (int j = 0; j < this.cols; j++) {
+                if (grid[i][j] == 1) {
+                    Deque<Point> queue = new ArrayDeque<Point>();
+                    boolean[][] visited = new boolean[this.rows][this.cols];
+                    queue.offer(new Point(i, j));
+                    visited[i][j] = true;
+                    int level = 1;
+                    while(!queue.isEmpty()) {
+                        int size = queue.size();
+                        
+                        for (int s = 0; s < size; s++) {
+                            Point cur = queue.poll();
+                            
+                            for (int k = 0; k < 4; k++) {
+                                int index_x = cur.x + dx[k];
+                                int index_y = cur.y + dy[k];
+                                
+                                if (index_x >= 0 && index_x < this.rows && index_y >= 0 && index_y < this.cols && !visited[index_x][index_y]) {
+                                    visited[index_x][index_y] = true;
+                                    queue.offer(new Point(index_x, index_y));
+                                    dist[index_x][index_y] += level;
+                                }
+                            }
+                        }
+                        level++;
+                    }
+                }
+            }
+        }
+        
+        int min = Integer.MAX_VALUE;
+        
+        for (int i = 0; i <this.rows; i++) {
+            for(int j = 0; j < this.cols; j++) {
+                
+                    min = Math.min(dist[i][j], min);
+                
+            }
+        }
+        
+        return min = min == Integer.MAX_VALUE? 1 : min;
+    }
+}
