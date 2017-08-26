8f8bf61175fcfc8c7e662dfcf5e47212da38b287
Jane Yuan <janeyuan1992@gmail.com>
2017-08-26 10:28:47 -0400

    Update PermutationI.java

--- backtracking/PermutationI.java
+++ backtracking/PermutationI.java
@@ -26,3 +26,41 @@ public class PermutationI {
         }
     }
 }
+
+/*
+                         1
+                 12            21
+            312 132 123   321 231 213
+
+Time Complexity : O(n! * n)
+
+*/
+class PermutationBFSSolution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> ret = new ArrayList<List<Integer>>();
+        if (nums == null || nums.length == 0) {
+            return ret;
+        }
+        
+        List<Integer> ans = new ArrayList<Integer>();
+        ret.add(ans);
+        
+        //i is from nums[0] to nums[nums.length - 1]
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> tmp = new ArrayList<List<Integer>>();
+            //iterate over the position to insert the new element
+            for (int j = 0; j <= i; j++) {
+                //iterate over the current result list
+                for (int k = 0; k < ret.size(); k++) {
+                    List<Integer> temp = new ArrayList<Integer>(ret.get(k));
+                    temp.add(j, nums[i]);
+                    tmp.add(temp);
+                }
+            }
+            
+            ret = tmp;
+        }
+        
+        return ret;
+    }
+}
