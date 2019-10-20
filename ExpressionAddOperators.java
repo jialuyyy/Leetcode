b6f134fb6ff54ddd4c0d982f2c29977edfd09857
Jane Yuan <janeyuan1992@gmail.com>
2019-10-20 11:45:38 -0400

    Update ExpressionAddOperators.java

--- ExpressionAddOperators.java
+++ ExpressionAddOperators.java
@@ -20,3 +20,47 @@ Example 5:
 
 Input: num = "3456237490", target = 9191
 Output: []*/
+
+//backtracking
+
+
+class Solution {
+    public List<String> addOperators(String num, int target) {
+        List<String> ret = new ArrayList<>();
+        
+        helper(num, target, "", 0, 0, 0, ret);
+        return ret;
+    }
+    //pos: current index we are operating on
+    //prevNum: used to operate on multiply operator. for 1 + 2 * 3, it will be 9, so we need to do (1 + 2 - 2) + 2 * 3
+    //to get 7
+    private void helper(String num, int target, String path, int pos, long curRes, long prevNum, List<String> ret) {
+        if (pos >= num.length()) {
+            if (curRes == target) {
+                ret.add(new String(path));
+                return;
+            }
+        }
+        
+        //i is used to build the substring. the outer level should be pos from 0 to num.length()
+        //and for each of the outer level, we need to backtrack and get all the operands combinations
+        for (int i = pos; i < num.length(); i++) {
+          
+            //if i != pos and num.charAt(pos） == ‘0’， which means we need to discard substrings like "05" starting from 0
+            if (i != pos && num.charAt(pos) == '0') {
+                return;
+            }
+            
+            String cur = num.substring(pos, i + 1);
+            long curNum = Long.parseLong(cur);
+            
+            if (pos != 0) {
+                helper(num, target, path + "+" + cur, i + 1, curRes + curNum, curNum, ret);
+                helper(num, target, path + "-" + cur, i + 1, curRes - curNum, -curNum, ret);
+                helper(num, target, path + "*" + cur, i + 1, (curRes - prevNum) + prevNum * curNum, prevNum * curNum, ret);
+            } else {
+                helper(num, target, cur, i + 1, curNum, curNum, ret);
+            }
+        }
+    }
+}
