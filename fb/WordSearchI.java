4a0943173a008af140f8669cffde075d215c12e6
Jane Yuan <janeyuan1992@gmail.com>
2019-10-25 09:19:55 -0400

    Update WordSearchI.java

--- fb/WordSearchI.java
+++ fb/WordSearchI.java
@@ -38,3 +38,42 @@ public class WordSearchI {
         
     }
 }
+
+// dont do up = i + 1, down = i - 1, left = j - 1, right = j + 1, and return the result as long as there is one boolean is true,
+//which will cause TLE
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board.length == 0)
+            return false;
+    
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == word.charAt(0))
+                    if (helper(i, j, board, word, 0))
+                        return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private boolean helper(int i, int j, char[][] board, String word, int index) {
+        if(index == word.length()) {
+            return true;
+        }
+        
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length || board[i][j] != word.charAt(index))
+            return false;
+            
+        board[i][j] = '#';
+        
+        if (helper(i + 1, j, board, word, index + 1) || helper(i - 1, j, board, word, index + 1) ||
+           helper(i, j + 1, board, word, index + 1) || helper(i, j - 1, board, word, index + 1))
+            return true;
+        
+        board[i][j] = word.charAt(index);
+        
+        return false;
+    }
+}
