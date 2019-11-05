3ff2ed190f153583367ed2e0b206b1ea60ad3405
Jane Yuan <janeyuan1992@gmail.com>
2019-11-05 08:35:09 -0500

    Update MaxProfitI.java

--- fb/MaxProfitI.java
+++ fb/MaxProfitI.java
@@ -2,24 +2,22 @@
 //for every price, compare the profit if sells at it and buys it from index 0 to index i
 //TLE for the last test case, so need to do some optimize
 
-public class MaxProfitI {
+class MaxProfitI {
     public int maxProfit(int[] prices) {
-        if (prices == null || prices.length <= 1) {
+        if (prices == null || prices.length == 0)
             return 0;
-        }
+         
+        int maxProfit = 0;
         
-        int max = 0;
-        int globalMax = 0;
         for (int i = 0; i < prices.length; i++) {
             for (int j = 0; j < i; j++) {
-                if (prices[i] > prices[j])
-                    max = Math.max(max, prices[i] - prices[j]);
+                if (prices[i] > prices[j]) {
+                    maxProfit = Math.max(maxProfit, prices[i] - prices[j]);
+                }
             }
-            
-            globalMax = Math.max(globalMax, max);
         }
         
-        return globalMax;
+        return maxProfit;
     }
 }
 
