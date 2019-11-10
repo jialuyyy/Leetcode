3aa717b8baba8341255f8297f9960f34ab6011cf
Jane Yuan <janeyuan1992@gmail.com>
2019-11-10 12:46:40 -0500

    Update NextPermutation.java

--- G/NextPermutation.java
+++ G/NextPermutation.java
@@ -49,3 +49,75 @@ public class nextPermutation {
         }
     }
 }
+
+//1 2 3
+//1 3 2
+//2 1 3
+//2 3 1 
+//3 1 2 
+//3 2 1
+
+
+// 1 2 3 4
+// 1 2 4 3
+// 1 3 2 4
+// 1 3 4 2
+// 1 4 2 3
+// 1 4 3 2
+// 2 1 3 4
+// 2 1 4 3
+// 2 3 1 4
+// 2 3 4 1
+// 2 4 1 3
+// 2 4 3 1
+
+//scan from the end
+//find the first element that is smaller than its next element
+//find the first element that is larger than the element found in first step
+//swap the two elements
+//reverse the elements from i + 1
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return;
+        
+        int i = nums.length - 2;
+        
+        //
+        while (i >= 0 && nums[i + 1] <= nums[i]) {
+            i--;
+        }
+        
+        if (i >= 0) {
+            int j = nums.length - 1;
+            //find the first element that is larger than current element
+            while (j >= 0 && nums[j] <= nums[i]) {
+                j--;
+            }
+            
+            swap(i, j, nums);
+        }
+        
+        //if the array is decreasing 
+        //just reverse
+        //4321 -> 1234
+        reverse(nums, i + 1, nums.length - 1);
+    }
+    
+    private void reverse(int[] nums, int start, int end) {
+        int i = start;
+        int j = end;
+        
+        while (i < j) {
+            swap(i, j, nums);
+            i++;
+            j--;
+        }
+    }
+    
+    private void swap(int i, int j, int[] nums) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
