ab9b9e59f584ba8de707b29335b6cfec5fccbd5b
Jane Yuan <janeyuan1992@gmail.com>
2019-11-15 09:46:41 -0500

    Update AddStrings.java

--- AddStrings.java
+++ AddStrings.java
@@ -38,3 +38,45 @@ class AddStrings {
         return ret;
     }
 }
+
+//use stringbuilder is much faster than using string
+class AddStrings {
+    public String addStrings(String num1, String num2) {
+        
+        if (num1 == null && num2 == null)
+            return "";
+        if (num1 == null && num2 != null)
+            return num2;
+        if (num1 != null && num2 == null)
+            return num1;
+        
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while (i >= 0 || j >= 0) {
+            int num = carry;
+            
+            if (i >= 0) {
+                num += (num1.charAt(i) - '0');
+                i--;
+            }
+            
+            if (j >= 0) {
+                num += (num2.charAt(j) - '0');
+                j--;
+            }
+            
+            int mod = num % 10;
+            carry = num / 10;
+            
+            sb.append(mod);
+        }
+        
+        if (carry > 0) {
+            sb.append(carry);
+        }
+        
+        return sb.reverse().toString();
+    }
+}
