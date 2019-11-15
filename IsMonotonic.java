45450824ef85c221c17b2fd00e83acfe0017614d
Jane Yuan <janeyuan1992@gmail.com>
2019-11-15 08:52:56 -0500

    Update IsMonotonic.java

--- IsMonotonic.java
+++ IsMonotonic.java
@@ -53,3 +53,24 @@ class Solution {
     
     
 }
+
+class Solution {
+    public boolean isMonotonic(int[] A) {
+        if (A == null || A.length == 0)
+            return false;
+       
+        boolean increasing = true;
+        boolean decreasing = true;
+        
+        for(int i = 0; i < A.length - 1; i++) {
+            if (A[i] > A[i + 1]) 
+                increasing = false;
+            if (A[i] < A[i + 1])
+                decreasing = false;
+        }
+        
+        return increasing || decreasing;
+    }
+    
+    
+}
