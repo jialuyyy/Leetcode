6e335c692048fc3274a0ad66b1ad6b8757d7fa66
Jane Yuan <janeyuan1992@gmail.com>
2019-11-15 09:19:56 -0500

    Update FirstBadVersion.java

--- thirdweek/FirstBadVersion.java
+++ thirdweek/FirstBadVersion.java
@@ -24,3 +24,30 @@ public class FirstBadVersion extends VersionControl {
         return end;
     }
 }
+
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+//TLE
+//when the input is the Integer.MAX_VALUE and the first bad version is also Integer.MAX_VALUE;
+//when mid = Integer.MAX_VALUE - 1;
+//start will be Integer.MAX_VALUE
+//then start + 1 will exceed the Integer range
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            
+            if (isBadVersion(mid)) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return isBadVersion(start) ? start : end;
+    }
+}
