44e7309deb8c3a741e2ad5f9bd709b228063391e
Jane Yuan <janeyuan1992@gmail.com>
2019-11-18 09:23:06 -0500

    Update IsBipartite.java

--- IsBipartite.java
+++ IsBipartite.java
@@ -51,3 +51,32 @@ class IsBipartite {
         
     }
 }
+class IsBipartite {
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] colors = new int[n];
+        
+        for (int i = 0; i < n; i++) {
+            if (colors[i] == 0 && !validColor(graph, colors, 1, i)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean validColor(int[][] graph, int[] colors, int color, int node) {
+        if (colors[node] != 0) {
+            return colors[node] == color;
+        }
+        
+        colors[node] = color;
+        for (int next : graph[node]) {
+            if (!validColor(graph, colors, -color, next)) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+}
