b00175c0e6b8ab908d794d6101ceeec8170ef8df
Jane Yuan <janeyuan1992@gmail.com>
2019-11-19 23:51:42 -0500

    Update SwapPairs.java

--- secondweek/SwapPairs.java
+++ secondweek/SwapPairs.java
@@ -30,3 +30,30 @@ public class SwapPairs {
         return dummy.next;
     }
 }
+
+//recursion
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        
+        ListNode node = head.next;
+        head.next = swapPairs(node.next);
+        node.next = head;
+        
+        return node;
+    }
+    
+    //1->2->3->4
+    
+    
+
+}
