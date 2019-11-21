4b96273dbada6b60af057a09e287add4b4075559
Jane Yuan <janeyuan1992@gmail.com>
2019-11-21 09:12:51 -0500

    Update KthGrammar.java

--- KthGrammar.java
+++ KthGrammar.java
@@ -14,3 +14,10 @@ class KthGrammar {
         
     }
 }
+
+class KthGrammar {
+    public int kthGrammar(int N, int K) {
+        if (N == 1) return 0;
+        return (~K & 1) ^ kthGrammar(N-1, (K+1)/2);
+    }
+}
