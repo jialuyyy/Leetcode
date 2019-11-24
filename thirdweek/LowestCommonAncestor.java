fcf308530483a7dab5804e190039baa9b38002ef
Jane Yuan <janeyuan1992@gmail.com>
2019-11-24 12:11:28 -0500

    Update LowestCommonAncestor.java

--- thirdweek/LowestCommonAncestor.java
+++ thirdweek/LowestCommonAncestor.java
@@ -37,3 +37,52 @@ public class LowestCommonAncestor {
         return null;
     }
 }
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+//iterative method
+class Solution {
+    
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        Deque<TreeNode> queue = new ArrayDeque<>();
+        Map<TreeNode, TreeNode> parentMap = new HashMap<>();
+        parentMap.put(root, null);
+        queue.offer(root);
+        
+        while( !parentMap.containsKey(p) || !parentMap.containsKey(q) ) {
+            TreeNode cur = queue.poll();
+            
+            if (cur.left != null) {
+                parentMap.put(cur.left, cur);
+                queue.offer(cur.left);
+            }
+            
+            if (cur.right != null) {
+                parentMap.put(cur.right, cur);
+                queue.offer(cur.right);
+            }
+        }
+        
+        Set<TreeNode> ancestors = new HashSet<>();
+        while (p != null) {
+            ancestors.add(p);
+            p = parentMap.get(p);
+        }
+        
+        while(!ancestors.contains(q)) {
+            q = parentMap.get(q);
+        }
+        
+        return q;
+    }
+    
+    
+}
