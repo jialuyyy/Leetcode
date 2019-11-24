8b7175c6d1f19aa786335ac8f234cd148d315838
Jane Yuan <janeyuan1992@gmail.com>
2019-11-24 16:13:48 -0500

    Update MinRemoveToMakeValid.java

--- MinRemoveToMakeValid.java
+++ MinRemoveToMakeValid.java
@@ -36,3 +36,24 @@ class MinRemoveToMakeValid {
         return sb.toString();
     }
 }
+
+//use place holder * to mark the removed character
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        if (s == null || s.length() == 0)
+            return s;
+        
+       StringBuilder sb = new StringBuilder(s);
+       Stack<Integer> st = new Stack<>();
+       for (int i = 0; i < sb.length(); ++i) {
+           if (sb.charAt(i) == '(') st.add(i);
+           if (sb.charAt(i) == ')') {
+           if (!st.empty()) st.pop();
+           else sb.setCharAt(i, '*');
+         }
+       }
+       while (!st.empty())
+           sb.setCharAt(st.pop(), '*');
+       return sb.toString().replaceAll("\\*", "");
+    }
+}
