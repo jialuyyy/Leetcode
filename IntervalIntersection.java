6fa4817a39152dd910bbcb6ec4f7c832c2ed348d
Jane Yuan <janeyuan1992@gmail.com>
2019-11-25 09:19:34 -0500

    Update IntervalIntersection.java

--- IntervalIntersection.java
+++ IntervalIntersection.java
@@ -1,3 +1,10 @@
+//[[0,2],[5,10],[13,23],[24,25]]
+//[[1,5],[8,12],[15,24],[25,26]]
+
+//for [0,2] and [1,5], we will discard [0,2] after the first intersection action
+//every time move the pointer forward if the ending point is smaller than the other one.
+//pick the larger element of the start and the smaller element of the end to get the intersection
+//Time Complexity: O(N) + O(M)
 class IntervalIntersection {
     public int[][] intervalIntersection(int[][] A, int[][] B) {
         if (A == null)
