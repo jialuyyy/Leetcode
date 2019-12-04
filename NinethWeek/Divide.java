9a999a879789b362253e3dac6f3ba712db1bd3fc
Jane Yuan <janeyuan1992@gmail.com>
2019-12-04 17:01:16 -0500

    Update Divide.java

--- NinethWeek/Divide.java
+++ NinethWeek/Divide.java
@@ -73,3 +73,37 @@ public class Divide {
         return ans;
     }
 }
+
+
+class Solution {
+    public int divide(int dividend, int divisor) {
+        if (divisor == 0)
+            return Integer.MAX_VALUE;
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+        
+        if (dividend == Integer.MIN_VALUE && divisor == 1)
+            return Integer.MIN_VALUE;
+        
+        int res = 0;
+        int sign = (dividend < 0) ^ (divisor < 0) ? -1 : 1;
+        
+        long dvd = Math.abs((long)dividend);
+        long dvs = Math.abs((long)divisor);
+        
+        while (dvs <= dvd) {
+            long temp = dvs;
+            long mul = 1;
+            
+            while (dvd >= temp << 1) {
+                temp = temp << 1;
+                mul = mul << 1;
+            }
+            
+            dvd = dvd - temp;
+            res += mul;
+        }
+        
+        return sign == 1 ? res : -res;
+    }
+}
