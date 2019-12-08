dbf71f5c5c6c25905e3b518fa86157173f6a6eac
Jane Yuan <janeyuan1992@gmail.com>
2019-12-08 18:19:43 -0500

    Update SetZeroes.java

--- SetZeroes.java
+++ SetZeroes.java
@@ -41,3 +41,55 @@ class Solution {
         
     }
 }
+
+
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        if (matrix == null || matrix.length == 0)
+            return;
+        
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        
+        boolean fr = false;
+        boolean fc = false;
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (matrix[i][j] == 0) {
+                    if (i == 0)
+                        fr = true;
+                    
+                    if (j == 0)
+                        fc = true;
+                    
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        
+        for (int i = 1; i < rows; i++) {
+            for (int j = 1; j < cols; j++) {
+                if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                   matrix[i][j] = 0;
+                }
+            }
+        }
+        
+        
+        if (fr) {
+            for (int i = 0; i < cols; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        
+        if (fc) {
+            for (int i = 0; i < rows; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+        
+        
+    }
+}
