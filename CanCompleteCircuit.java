439da6fe207830eaaf93cb464dbe9f286092f671
Jane Yuan <janeyuan1992@gmail.com>
2019-12-15 12:34:43 -0500

    Create CanCompleteCircuit.java

---
+++ CanCompleteCircuit.java
@@ -0,0 +1,18 @@
+class CanCompleteCircuit {
+    public int canCompleteCircuit(int[] gas, int[] cost) {
+        int total = 0;
+        int cur = 0;
+        int start = 0;
+        for (int i = 0; i < gas.length; i++) {
+            total += gas[i] - cost[i];
+            cur += gas[i] - cost[i];
+            
+            if (cur < 0) {
+                start = i + 1;
+                cur = 0;
+            }
+        }
+        
+        return total >= 0 ? start : -1;
+    }
+}
