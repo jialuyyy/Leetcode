338a849b7b1b03901b0cf8df1fc21f3e14825efd
Jane Yuan <janeyuan1992@gmail.com>
2019-12-20 16:25:25 -0500

    Update FindRestaurant.java

--- FindRestaurant.java
+++ FindRestaurant.java
@@ -86,3 +86,36 @@ class FindRestaurant {
             
     }
 }
+
+
+class Solution {
+    public String[] findRestaurant(String[] list1, String[] list2) {
+        if (list1 == null || list2 == null || list1.length == 0 || list2.length == 0) {
+            return new String[]{};
+        }
+       
+        Map<String, Integer> map = new HashMap<>();
+        for (int i = 0; i < list1.length; i++) {
+            map.put(list1[i], i);
+        }
+        
+        List<String> res = new ArrayList<>();
+        int min_sum = Integer.MAX_VALUE;
+        int sum = 0;
+        
+        for (int i = 0; i < list2.length && i <= min_sum; i++) {
+            if (map.containsKey(list2[i])) {
+                sum = i + map.get(list2[i]);
+                if (sum < min_sum) {
+                    res.clear();
+                    res.add(list2[i]);
+                    min_sum = sum;
+                } else if (sum == min_sum) {
+                    res.add(list2[i]);
+                }
+            }
+        }
+        
+        return res.toArray(new String[res.size()]);
+    }
+}
