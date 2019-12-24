77e4383cb6dba5ac88fc11fc4f852975b5bac5ee
Jane Yuan <janeyuan1992@gmail.com>
2019-12-24 12:57:24 -0500

    Update DailyTemperatures.java

--- DailyTemperatures.java
+++ DailyTemperatures.java
@@ -14,3 +14,21 @@ class DailyTemperatures {
         return ret;
     }
 }
+
+
+class DailyTemperatures {
+    public int[] dailyTemperatures(int[] T) {
+        int[] ret = new int[T.length];
+        Deque<Integer> stack = new ArrayDeque<>();
+        
+        for (int i = T.length - 1; i >= 0; i--) {
+            while (!stack.isEmpty() && T[i] >= T[stack.peek()])
+                stack.pop();
+            
+            ret[i] = stack.isEmpty() ? 0 : stack.peek() - i;
+            stack.push(i);
+        }
+        
+        return ret;
+    }
+}
