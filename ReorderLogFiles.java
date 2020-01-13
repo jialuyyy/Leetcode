2c331ad6019fa3c4aa270210d5b942ed34fd679d
Jane Yuan <janeyuan1992@gmail.com>
2020-01-13 08:44:33 -0500

    Update ReorderLogFiles.java

--- ReorderLogFiles.java
+++ ReorderLogFiles.java
@@ -25,3 +25,45 @@ class ReorderLogFiles {
         return logs;
     }
 }
+
+
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        if (logs == null || logs.length == 0)
+            return logs;
+        
+        String[] ret = new String[logs.length];
+        List<String> list = new ArrayList<>();
+        int index = ret.length - 1;
+        for (int i = logs.length - 1; i >= 0; i--) {
+            String substring = logs[i].substring(logs[i].indexOf(" ") + 1);
+            
+            if (Character.isDigit(substring.charAt(0))) {
+                ret[index--] = logs[i];
+            } else {
+                list.add(logs[i]);
+            }
+        }
+        
+        Collections.sort(list, new LogComparator());
+        
+        for (int i = 0; i < list.size(); i++) {
+            ret[i] = list.get(i);
+        }
+        
+        return ret;
+    }
+    
+    public class LogComparator implements Comparator<String> {
+        public int compare(String s1, String s2) {
+            String substring1 = s1.substring(s1.indexOf(" ") + 1);
+            String substring2 = s2.substring(s2.indexOf(" ") + 1);
+            
+            if (!substring1.equals(substring2)) {
+                return substring1.compareTo(substring2);
+            } else {
+                return s1.substring(0, s1.indexOf(" ")).compareTo(s2.substring(0, s2.indexOf(" ")));
+            }
+        }
+    }
+}
