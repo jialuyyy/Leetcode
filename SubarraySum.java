38080c6c7c43a75b847db594e6c58bf7865ec0c7
Jane Yuan <janeyuan1992@gmail.com>
2020-01-14 16:07:36 -0500

    Update SubarraySum.java

--- SubarraySum.java
+++ SubarraySum.java
@@ -1,3 +1,25 @@
+//time limit exceeds
+class subarraySum
+{
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = 0;
+                for (int i = start; i < end; i++) {
+                    sum += nums[i];
+                }
+                
+                if (sum == k)
+                    count++;
+            }
+        }
+        
+        return count;
+    }
+}
+
+
 //[1,1,1]
 //presum[0] = 0; presum[1] = 1; presum[2] = 2;presum[3] = 3
 
