70cc96e22975d1a546a9462e2ceb091d7fb03a03
Jane Yuan <janeyuan1992@gmail.com>
2020-01-23 11:18:43 -0500

    Update CheckSubarraySum.java

--- CheckSubarraySum.java
+++ CheckSubarraySum.java
@@ -28,3 +28,29 @@ class CheckSubarraySum {
        
     }
 }
+
+//O(n ^ 2)
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        if (nums == null || nums.length == 0)
+            return false;
+  
+        for (int i = 0; i < nums.length; i++) {
+            int sum = nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
+                sum += nums[j];
+                
+                if (k == 0) {
+                    if (sum == 0)
+                        return true;
+                    else
+                        continue;
+                }
+                if (sum % k == 0)
+                    return true;
+            }
+        }
+        
+        return false;
+    }
+}
