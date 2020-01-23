978e3eaa29e4a67726d9958fddc1fc6057816329
Jane Yuan <janeyuan1992@gmail.com>
2020-01-22 21:27:40 -0500

    Update Intersection.java

--- Intersection.java
+++ Intersection.java
@@ -76,3 +76,33 @@ class Solution {
         return ret;
     }
 }
+
+class Solution {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        
+        int n1 = 0;
+        int n2 = 0;
+        
+        int len = nums1.length < nums2.length ? nums1.length : nums2.length;
+        int[] ret = new int[len];
+        int index = 0;
+        while (n1 < nums1.length && n2 < nums2.length) {
+            if (nums1[n1] == nums2[n2]) {
+                int temp = nums1[n1];
+                ret[index++] = nums1[n1];
+                while (n1 < nums1.length && nums1[n1] == temp)
+                    n1++;
+                while (n2 < nums2.length && nums2[n2] == temp)
+                    n2++;
+            } else if (nums1[n1] < nums2[n2]) {
+                n1++;
+            } else {
+                n2++;
+            }
+        }
+        
+        return Arrays.copyOf(ret, index);
+    }
+}
