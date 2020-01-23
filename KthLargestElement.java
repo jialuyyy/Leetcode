53bf6d51e05867795c79f7a5353823c730ca44fc
Jane Yuan <janeyuan1992@gmail.com>
2020-01-22 20:53:58 -0500

    Update KthLargestElement.java

--- KthLargestElement.java
+++ KthLargestElement.java
@@ -27,3 +27,67 @@ class Solution {
         return pq.poll();
     }
 }
+
+class FindKthLargest {
+    public int findKthLargest(int[] nums, int k) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        
+        return helper(nums, 0, nums.length - 1, nums.length - k);
+    }
+    
+    private int helper(int[] nums, int start, int end, int k) {
+        
+        
+        while (start < end) {
+            int p = partition(nums, start, end);
+            if (p < k) {
+                start = p + 1;
+            } else if (p > k) {
+                end = p - 1;
+            } else {
+                return nums[p];
+            }
+        }
+        
+        
+        return nums[k];
+    }
+    
+    private int partition(int[] nums, int start, int end) {
+        int low = start;
+        int high = end + 1;
+        
+        int pivot = nums[start];
+        
+        while (true) {
+            while (nums[++low] <= pivot) {
+                if (low >= end) {
+                    break;
+                }
+            }
+            
+            while (nums[--high] >= pivot) {
+                if (high <= start) {
+                    break;
+                }
+            }
+            
+            if (low >= high) {
+                break;
+            }
+            
+            swap(nums, low, high);
+        }
+        
+        swap(nums, high, start);
+        
+        return high;
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
