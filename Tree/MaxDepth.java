20402ee973e06b229581fbd311fc8e8a720c9577
Jane Yuan <janeyuan1992@gmail.com>
2020-01-23 21:20:18 -0500

    Update MaxDepth.java

--- Tree/MaxDepth.java
+++ Tree/MaxDepth.java
@@ -19,3 +19,33 @@ public class MaxDepth {
         return Math.max(left, right) + 1;
     }
 }
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class MaxDepth {
+    private int height = 0;
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        
+        helper(1, root);
+        return height;
+    }
+    
+    private void helper(int cur, TreeNode root) {
+        if (root == null)
+            return;
+        
+        if (height < cur) {
+            height = cur;
+        }
+        helper(cur + 1, root.left);
+        helper(cur + 1, root.right);
+    }
+}
