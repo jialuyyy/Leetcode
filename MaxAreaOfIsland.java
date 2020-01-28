edbb148a7e7af00e2d8db53ab8b9ac41d0bdff24
Jane Yuan <janeyuan1992@gmail.com>
2020-01-27 21:06:11 -0500

    Update MaxAreaOfIsland.java

--- MaxAreaOfIsland.java
+++ MaxAreaOfIsland.java
@@ -30,3 +30,35 @@ class MaxAreaOfIsland {
         return 1 + dfs(grid, i + 1, j, visited) + dfs(grid, i - 1, j, visited) + dfs(grid, i, j - 1, visited) + dfs(grid, i, j + 1, visited);
     }
 }
+
+
+class Solution {
+    private int maxArea = 0;
+    private boolean[][]  visited = null;
+    public int maxAreaOfIsland(int[][] grid) {
+        if (grid == null || grid.length == 0 || grid[0].length == 0)
+            return 0;
+        
+        int rows = grid.length;
+        int cols = grid[0].length;
+        visited = new boolean[rows][cols];
+        
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                maxArea = Math.max(maxArea, dfs(grid, i, j));
+            }
+            
+        }
+        
+        return maxArea;
+    }
+    
+    private int dfs(int[][] grid, int i, int j) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || visited[i][j] || grid[i][j] == 0) {
+            return 0;
+        }
+        
+        visited[i][j] = true;
+        return 1 + dfs(grid, i + 1, j) + dfs(grid, i - 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1);
+    }
+}
