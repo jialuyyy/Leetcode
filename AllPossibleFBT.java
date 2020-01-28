c5de66329e6d4cb5182467d0345a7d57b574d4d7
Jane Yuan <janeyuan1992@gmail.com>
2020-01-27 19:13:06 -0500

    Update AllPossibleFBT.java

--- AllPossibleFBT.java
+++ AllPossibleFBT.java
@@ -36,3 +36,46 @@ class AllPossibleFBT {
         return ret;
     }
 }
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    Map<Integer, List<TreeNode>> cache = new HashMap<>();
+    public List<TreeNode> allPossibleFBT(int N) {
+        List<TreeNode> ret = new ArrayList<>();
+        if (N % 2 == 0)
+            return ret;
+        
+        if (cache.containsKey(N)) {
+            return cache.get(N);
+        }
+        if (N == 1) {
+            TreeNode node = new TreeNode(0);
+            ret.add(node);
+            return ret;
+        }
+        N = N - 1;
+        
+        for (int i = 1; i <= N; i += 2) {
+            List<TreeNode> left = allPossibleFBT(i);
+            List<TreeNode> right = allPossibleFBT(N - i);
+            for (TreeNode l : left) {
+                for (TreeNode r : right) {
+                    TreeNode root = new TreeNode(0);
+                    root.left = l;
+                    root.right = r;
+                    ret.add(root);
+                }
+            }
+        }
+        cache.put(N, ret);
+        return ret;
+    }
+}
