acc413ec0477797925b2aa9c475cd0747cea2307
Jane Yuan <janeyuan1992@gmail.com>
2020-01-28 21:26:31 -0500

    Create FindCircleNum.java

---
+++ FindCircleNum.java
@@ -0,0 +1,30 @@
+class FindCircleNum {
+    public int findCircleNum(int[][] M) {
+        if (M == null || M.length == 0 || M[0].length == 0)
+            return 0;
+        
+        int rows = M.length;
+        int cols = M[0].length;
+        
+        int count = 0;
+        int[] visited = new int[M.length];
+        
+        for (int i = 0; i < rows; i++) {
+            if (visited[i] == 0) {
+                dfs(M, visited, i);
+                count++;
+            }
+        }
+
+        return count;
+    }
+    
+    private void dfs(int[][] M ,int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            } 
+        }
+    }
+}
