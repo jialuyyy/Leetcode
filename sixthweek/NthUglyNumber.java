1d161fe6c1a81a0632dc4ad30f46e3d5ee1a8eb7
Jane Yuan <janeyuan1992@gmail.com>
2020-01-30 09:10:44 -0500

    Update NthUglyNumber.java

--- sixthweek/NthUglyNumber.java
+++ sixthweek/NthUglyNumber.java
@@ -67,3 +67,32 @@ public class Solution {
         return ugly[n - 1];
     }
 }
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        if ( n == 0 )
+            return 0;
+        if (n == 1)
+            return 1;
+        
+        int t1 = 0;
+        int t2 = 0;
+        int t3 = 0;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.min(Math.min(dp[t1] * 2, dp[t2] * 3), dp[t3] * 5);
+            
+            if (dp[i] == dp[t1] * 2)
+                t1++;
+             
+            if (dp[i] == dp[t2] * 3)
+                t2++;
+             
+            if (dp[i] == dp[t3] * 5)
+                t3++;
+        }
+        
+        return dp[n - 1];
+    }
+}
