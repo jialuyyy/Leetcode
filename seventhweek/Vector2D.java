046facf57e2bbfedd892b5730632a84c63bb5956
Jane Yuan <janeyuan1992@gmail.com>
2020-01-31 11:19:58 -0500

    Update Vector2D.java

--- seventhweek/Vector2D.java
+++ seventhweek/Vector2D.java
@@ -32,3 +32,46 @@ public class Vector2D implements Iterator<Integer> {
  * Vector2D i = new Vector2D(vec2d);
  * while (i.hasNext()) v[f()] = i.next();
  */
+
+
+class Vector2D {
+    private Deque<int[]> stack = null;
+    public Vector2D(int[][] v) {
+        stack = new ArrayDeque<>();
+        
+        for (int i = v.length - 1; i >= 0; i--) {
+            if (v[i] != null && v[i].length > 0)
+                stack.push(v[i]);
+        } 
+    }
+    
+    public int next() {
+        if (!hasNext())
+            return -1;
+        
+        return stack.poll()[0];
+    }
+    
+    public boolean hasNext() {
+        while (!stack.isEmpty()) {
+            int[] cur = stack.peek();
+            
+            if (cur.length == 1) {
+                return true;
+            }
+            
+            stack.poll();
+            for (int i = cur.length - 1; i >= 0; i--) {
+                stack.push(new int[]{cur[i]});
+            }
+        }
+        return false;
+    }
+}
+
+/**
+ * Your Vector2D object will be instantiated and called as such:
+ * Vector2D obj = new Vector2D(v);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
