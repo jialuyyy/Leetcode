4851d70d36421872793b740fd9756780615f0098
Jane Yuan <janeyuan1992@gmail.com>
2020-02-01 16:11:01 -0500

    Update topKFrequentWords.java

--- topKFrequentWords.java
+++ topKFrequentWords.java
@@ -37,3 +37,44 @@ class topKFrequentWords {
         return ret;
     }
 }
+
+//just need to store strings int priority queue, no need to store entry
+//maintain a min heap and the small frequency will be before the larger frequency and when the frequency of two words equal ,
+//we put the larger words before the smaller workds
+//at the end, reverse the order
+class topKFrequentWord {
+    public List<String> topKFrequent(String[] words, int k) {
+        List<String> ret = new ArrayList<>();
+        if (words == null || words.length == 0)
+            return ret;
+        
+        //O(n) time
+        //O(n) space
+        Map<String, Integer> frequency = new HashMap<>();
+        for (String word : words) {
+            frequency.put(word, frequency.getOrDefault(word, 0) + 1);
+        }
+        
+        //min heap k size 
+        Queue<String> pq = new PriorityQueue<>(new Comparator<String>(){
+            public int compare(String s1, String s2) {
+                return frequency.get(s1) == frequency.get(s2) ? s2.compareTo(s1) : frequency.get(s1) - frequency.get(s2) ;
+            }
+        });
+        
+        for (String word : frequency.keySet()) {
+            pq.offer(word);
+            //minheap, the smallest will get out
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+        
+        while(!pq.isEmpty()) {
+            ret.add(pq.poll());
+        }
+        
+        Collections.reverse(ret);
+        return ret;
+    }
+}
