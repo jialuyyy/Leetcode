7432d605d23bcfc7025c2dad76493df0f7ea0da3
Jane Yuan <janeyuan1992@gmail.com>
2020-02-02 08:40:30 -0500

    Update FindAnagrams.java

--- FindAnagrams.java
+++ FindAnagrams.java
@@ -28,3 +28,47 @@ class FindAnagrams {
         return ret;
     }
 }
+
+class FindAnagrams {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+        if (s.length() < p.length())
+            return ans;
+        
+        char[] sc = s.toCharArray();
+        char[] pc = p.toCharArray();
+        
+        int[] det = new int[256];
+        
+        for (int i = 0; i < p.length(); i++) {
+            det[pc[i]]--;
+            det[sc[i]]++;
+        }
+        int absSum = 0;
+        
+        for (int item : det) {
+            absSum += Math.abs(item);
+        }
+        
+        if (absSum == 0)
+            ans.add(0);
+        
+        for (int i = p.length(); i < s.length(); i++) {
+            int r = sc[i];
+            int l = sc[i - p.length()];
+            
+            absSum = absSum - Math.abs(det[r]) - Math.abs(det[l]);
+            
+            //update the hash
+            det[r]++;
+            det[l]--;
+            
+            absSum = absSum + Math.abs(det[r]) + Math.abs(det[l]);
+            
+            if (absSum == 0)
+                ans.add(i - p.length() + 1);
+        }
+        
+        return ans;
+    }
+}
