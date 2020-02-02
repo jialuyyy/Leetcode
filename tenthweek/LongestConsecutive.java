26fc8d105e93fce9951a879a61a0d50c45b96d71
Jane Yuan <janeyuan1992@gmail.com>
2020-02-02 09:33:08 -0500

    Update LongestConsecutive.java

--- tenthweek/LongestConsecutive.java
+++ tenthweek/LongestConsecutive.java
@@ -30,3 +30,36 @@ public class LongestConsecutive {
         return max == Integer.MIN_VALUE? 1: max;
     }
 }
+
+//sort firstly
+//1,2,3,4 100 ,200
+//special case duplciate value
+//[0,1,1,2]
+//O(nlog(n))
+class LongestConsecutive {
+    public int longestConsecutive(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        
+        //O(nlog(n))
+        Arrays.sort(nums);
+        
+        int maxLen = 1;
+        int currentLen = 1;
+        
+        for (int i = 1; i < nums.length; i++) {
+            //skip the same value
+            if (nums[i] != nums[i - 1]) {
+                if (nums[i] - nums[i - 1] == 1) {
+                    currentLen += 1;
+                } else {
+                    maxLen = Math.max(currentLen, maxLen);
+                    currentLen = 1;
+                }
+                
+            }
+        }
+        
+        return Math.max(maxLen, currentLen);
+    }
+}
