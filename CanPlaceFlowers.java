9782b2cdbf5e01ccfdf16c938fccbd150fa436c5
Jane Yuan <janeyuan1992@gmail.com>
2020-02-08 09:34:20 -0500

    Create CanPlaceFlowers.java

---
+++ CanPlaceFlowers.java
@@ -0,0 +1,22 @@
+class CanPlaceFlowers {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int i = 0;
+        
+        int count = 0;
+        while (i < flowerbed.length) {
+            if (flowerbed[i] == 0) {
+                if ((i == 0 || flowerbed[i - 1] == 0) && 
+                   (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
+                    flowerbed[i] = 1;
+                    count++;
+                }
+            }
+            if (count >= n) {
+                return true;
+            }
+            i++;
+        }
+        
+        return count >= n;
+    }
+}
