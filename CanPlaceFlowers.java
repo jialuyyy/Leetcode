7b783b13e7360df3a5d7b732eff43d1031e8fc25
Jane Yuan <janeyuan1992@gmail.com>
2020-02-08 09:36:36 -0500

    Update CanPlaceFlowers.java

--- CanPlaceFlowers.java
+++ CanPlaceFlowers.java
@@ -3,7 +3,7 @@ class CanPlaceFlowers {
         int i = 0;
         
         int count = 0;
-        while (i < flowerbed.length) {
+        while (i < flowerbed.length && count < n) {
             if (flowerbed[i] == 0) {
                 if ((i == 0 || flowerbed[i - 1] == 0) && 
                    (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
@@ -11,12 +11,12 @@ class CanPlaceFlowers {
                     count++;
                 }
             }
-            if (count >= n) {
-                return true;
-            }
+        
             i++;
         }
         
-        return count >= n;
+        return count == n;
     }
 }
+
+
