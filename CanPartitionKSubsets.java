8def867d059fa3dbb495cec015716ac1a6465caf
Jane Yuan <janeyuan1992@gmail.com>
2020-02-08 11:02:03 -0500

    Update CanPartitionKSubsets.java

--- CanPartitionKSubsets.java
+++ CanPartitionKSubsets.java
@@ -33,3 +33,39 @@ class CanPartitionKSubsets {
         return false;
     }
 }
+
+class CanPartitionKSubsets {
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        if (nums == null || nums.length == 0)
+            return false;
+        
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        
+        if (k <= 0 || sum % k != 0)
+            return false;
+        
+        int[] visited = new int[nums.length];
+        return canPartition(nums, visited, 0, k, 0, sum / k);
+    }
+    
+    private boolean canPartition(int[] nums, int[] visited, int start_index, int k, int cur_sum, int target) {
+        if (k == 1) 
+            return true;
+        
+        if (cur_sum == target) 
+            return canPartition(nums, visited, 0, k - 1, 0, target);
+        
+        for (int i = start_index; i < nums.length; i++) {
+            if (visited[i] == 0 && cur_sum + nums[i] <= target) {
+                visited[i] = 1;
+                if (canPartition(nums, visited, i + 1, k, cur_sum + nums[i], target))
+                    return true;
+                visited[i] = 0;
+            }
+        }
+        return false;
+    }
+}
