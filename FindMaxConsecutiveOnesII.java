4dc9b8e234029f4ec564ac69bb1701d9cf39012f
Jane Yuan <janeyuan1992@gmail.com>
2020-02-08 12:48:01 -0500

    Update FindMaxConsecutiveOnesII.java

--- FindMaxConsecutiveOnesII.java
+++ FindMaxConsecutiveOnesII.java
@@ -21,3 +21,27 @@ class FindMaxConsecutiveOnesII {
         return max;
     }
 }
+
+class FindMaxConsecutiveOnesII {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int k = 1;
+        Deque<Integer> queue = new ArrayDeque<>();
+        
+        for (int l = 0, h = 0; h < nums.length; h++) {
+            if (nums[h] == 0) {
+                queue.offer(h);
+            }
+            
+            //only need to keep the index of num[index] = 0
+            //instead of keep all of the records
+            if (queue.size() > k) {
+                l = queue.poll() + 1;
+            }
+            
+            max = Math.max(max, h - l + 1);
+        }  
+        
+        return max;
+    }
+}
