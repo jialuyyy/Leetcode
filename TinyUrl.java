554d565b6af1b691fa1164cb58392845d197b704
Jane Yuan <janeyuan1992@gmail.com>
2020-02-12 09:22:19 -0500

    Update TinyUrl.java

--- TinyUrl.java
+++ TinyUrl.java
@@ -29,3 +29,43 @@ public class TinyUrl {
 // Your Codec object will be instantiated and called as such:
 // Codec codec = new Codec();
 // codec.decode(codec.encode(url));
+
+
+public class TinyUrl {
+    static String alphabet = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    HashMap<String, String> map = new HashMap<>();
+    HashMap<String, String> reverseMap = new HashMap<>();
+    final static String BASE_URL = "http://tinyurl.com/";
+    Random rand = new Random();
+    String key = getRand();
+
+    public String getRand() {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 6; i++) {
+            sb.append(alphabet.charAt(rand.nextInt(62)));
+        }
+        return sb.toString();
+    }
+
+    public String encode(String longUrl) {
+        //return if we have already created one shorturl for the specific longurl
+        if (reverseMap.containsKey(longUrl)) {
+            return BASE_URL + reverseMap.get(longUrl);
+        }
+        while (map.containsKey(key)) {
+            key = getRand();
+        }
+        map.put(key, longUrl);
+        reverseMap.put(longUrl, key);
+        return BASE_URL + key;
+    }
+
+    public String decode(String shortUrl) {
+        return map.get(shortUrl.replace(BASE_URL, ""));
+    }
+}
+
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(url));
