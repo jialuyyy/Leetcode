70964e240d6417d5e86726b162a8c1c5448a9d5a
Jane Yuan <janeyuan1992@gmail.com>
2020-02-15 08:40:06 -0500

    Update RandomIndex.java

--- RandomIndex.java
+++ RandomIndex.java
@@ -35,3 +35,46 @@ class RandomIndex {
  * Solution obj = new Solution(w);
  * int param_1 = obj.pickIndex();
  */
+
+
+class Solution {
+    private int[] accumulated = null;
+    private Random random = new Random();
+    public Solution(int[] w) {
+        if (w == null || w.length == 0)
+            return;
+        accumulated = new int[w.length];
+        accumulated[0] = w[0];
+        for (int i = 1; i < w.length; i++) {
+            accumulated[i] = w[i] + accumulated[i - 1];
+        }
+    }
+    
+    //binary search to improve to O(logn)
+    public int pickIndex() {
+        //get the random value between 0(inclusive) and accumulated[accumulated.length - 1](exclusive)
+        int rand = random.nextInt(accumulated[accumulated.length - 1]);
+        
+        //map it to the index
+        int start = 0;
+        int end = accumulated.length - 1;
+        
+        while (start + 1 < end) {
+            int mid = start + (end - start) / 2;
+            if (rand < accumulated[mid]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        
+        return rand < accumulated[start] ? start : end;
+        
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(w);
+ * int param_1 = obj.pickIndex();
+ */
